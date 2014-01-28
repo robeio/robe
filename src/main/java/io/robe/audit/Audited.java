359ed6c14d921504054a2060214bd30cd33ab10b
Seray Uzgur <seray.uzgur@mebitech.com>
2014-01-28 18:25:43 +0200

    ROBE-1 javadocs

--- src/main/java/io/robe/audit/Audited.java
+++ src/main/java/io/robe/audit/Audited.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation class for extra logging of methods (Resource)
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Audited {
