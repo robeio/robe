fa76f74b41cdeb8266040d4910133d627e95f143
Seray Uzgur <seray.uzgur@mebitech.com>
2014-01-29 14:29:05 +0200

    ROBE-1 javadocs

--- src/main/java/io/robe/auth/AuthInjectable.java
+++ src/main/java/io/robe/auth/AuthInjectable.java
@@ -2,6 +2,8 @@ package io.robe.auth;
 
 import com.google.common.base.Optional;
 import com.sun.jersey.api.core.HttpContext;
+import com.sun.jersey.api.uri.UriTemplate;
+import com.sun.jersey.server.impl.application.WebApplicationContext;
 import com.sun.jersey.server.impl.inject.AbstractHttpContextInjectable;
 import com.yammer.dropwizard.auth.AuthenticationException;
 import com.yammer.dropwizard.auth.Authenticator;
@@ -11,21 +13,39 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
-public class AuthInjectable<T> extends AbstractHttpContextInjectable {
+
+/**
+ * An {@link com.yammer.dropwizard.auth.Auth} injectable that obtains an injectable value given the HttpContext.
+ * Authenticate and Authorize controls are executed by this class on every inject request.
+ *
+ * @param <T> The type of the injectable.
+ */
+public class AuthInjectable<T extends Credentials> extends AbstractHttpContextInjectable<T> {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AuthInjectable.class);
 
-	private final Authenticator<String, Credentials> authenticator;
-	private final boolean required;
+	private final Authenticator<String, T> authenticator;
 
-	protected AuthInjectable(Authenticator<String, Credentials> authenticator, boolean required) {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @param authenticator The authenticator which will be used with Authenticate controls.
+	 */
+	protected AuthInjectable(Authenticator<String, T> authenticator) {
 		this.authenticator = authenticator;
-		this.required = required;
 	}
 
+	/**
+	 * This method gets the context and does all necessary controls.
+	 * Returns injectable or throws {@link javax.ws.rs.WebApplicationException} with responce type {@link Response.Status} UNAUTHORIZED
+	 *
+	 * @param c The http context of the inject request.
+	 * @return Returns the desired injectable.
+	 */
 	@Override
-	public Credentials getValue(HttpContext c) {
+	public T getValue(HttpContext c) {
 
 		Cookie tokenList = c.getRequest().getCookies().get("auth-token");
 		if (tokenList == null || tokenList.getValue().length() == 0) {
@@ -38,34 +58,39 @@ public class AuthInjectable<T> extends AbstractHttpContextInjectable {
 		}
 		//Validate old token for auth token
 		try {
+			Optional<T> result = authenticator.authenticate(token);
+			if (!result.isPresent())
+				throw new WebApplicationException(Response.Status.UNAUTHORIZED);
 
-			String sentence = token;
-			if (sentence != null) {
-				Optional<Credentials> result = authenticator.authenticate(sentence);
-				if (result.isPresent()) {
-					if(!isAuthorized(result.get(), "/" + c.getRequest().getPath() + ":" + c.getRequest().getMethod()))
-						throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+			if (!isAuthorized(result.get(), ((WebApplicationContext) c).getMatchedTemplates(), c.getRequest().getMethod()))
+				throw new WebApplicationException(Response.Status.UNAUTHORIZED);
 
-					return result.get();
-
-				}
-
-
-			}
+			return result.get();
 		} catch (IllegalArgumentException e) {
 			LOGGER.debug("BasicPair decoding credentials", e);
+			throw new WebApplicationException(Response.Status.PRECONDITION_FAILED);
 		} catch (AuthenticationException e) {
 			LOGGER.warn("BasicPair authenticating credentials", e);
 			throw new WebApplicationException(Response.Status.PRECONDITION_FAILED);
 		}
-
-		if (required) {
-			throw new WebApplicationException(Response.Status.UNAUTHORIZED);
-		}
-		return null;
 	}
 
-	private boolean isAuthorized(Credentials credentials, String service) {
-		return credentials.getPermissions().contains(service);
+
+	/**
+	 * Matches path pattern  and merges it for comparing with path annotations value.
+	 *
+	 * @param credentials      for checking permission list
+	 * @param matchedTemplates matched templates of context. They will be merged with reverse order
+	 * @param method           HTTP Method of the request. Will be merged with
+	 * @return true if user is Authorized.
+	 */
+	private boolean isAuthorized(Credentials credentials, List<UriTemplate> matchedTemplates, String method) {
+		StringBuilder path = new StringBuilder();
+		for (UriTemplate template : matchedTemplates)
+			path.insert(0, template.getTemplate());
+		path.append(":").append(method);
+
+		return credentials.getPermissions().contains(path.toString());
 	}
+
 }
--- src/main/java/io/robe/auth/AuthProvider.java
+++ src/main/java/io/robe/auth/AuthProvider.java
@@ -9,24 +9,45 @@ import com.sun.jersey.spi.inject.InjectableProvider;
 import com.yammer.dropwizard.auth.Auth;
 import com.yammer.dropwizard.auth.Authenticator;
 
-public class AuthProvider<T> implements InjectableProvider<Auth, Parameter> {
+/**
+ * Injectable provider for {@link com.yammer.dropwizard.auth.Auth} annotation.
+ * {@inheritDoc}
+ *
+ * @param <T> Type of the injectable parameter.
+ */
+public class AuthProvider<T extends Credentials> implements InjectableProvider<Auth, Parameter> {
 
-	@Inject
+
 	private  Authenticator authenticator;
 
 	/**
 	 * Creates a new AuthProvider with the given {@link Authenticator}
+	 * @param authenticator  Desired Authenticator to provide.
 	 */
-	public AuthProvider() {
+	@Inject
+	public AuthProvider(Authenticator authenticator) {
+		this.authenticator = authenticator;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * @return
+	 */
 	@Override
 	public ComponentScope getScope() {
 		return ComponentScope.PerRequest;
 	}
 
+
+	/**
+	 * {@inheritDoc}
+	 * @param context context of the component
+	 * @param auth annotation
+	 * @param parameter Injectable parameter
+	 * @return Returns an instance of {@link io.robe.auth.AuthInjectable}.
+	 */
 	@Override
-	public Injectable<?> getInjectable(ComponentContext ic, Auth a, Parameter c) {
-		return new AuthInjectable<T>(authenticator, a.required());
+	public Injectable getInjectable(ComponentContext context, Auth auth, Parameter parameter) {
+		return new AuthInjectable<T>(authenticator);
 	}
 }
