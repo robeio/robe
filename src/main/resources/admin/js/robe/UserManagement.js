371df0b0c8e05d025363b7ef04c33633f8608602
Seray Uzgur <seray.uzgur@mebitech.com>
2014-01-31 23:24:12 +0200

    ROBE-8

--- src/main/resources/admin/js/robe/UserManagement.js
+++ src/main/resources/admin/js/robe/UserManagement.js
@@ -3,25 +3,25 @@ function initializeUserManagement() {
         transport: {
             read: {
                 type: "GET",
-                url: getBackendURL()+"user/all",
+                url: getBackendURL() + "user/all",
                 dataType: "json",
                 contentType: "application/json"
             },
             update: {
                 type: "POST",
-                url: getBackendURL()+"user",
+                url: getBackendURL() + "user",
                 dataType: "json",
                 contentType: "application/json"
             },
             destroy: {
                 type: "DELETE",
-                url: getBackendURL()+"user",
+                url: getBackendURL() + "user",
                 dataType: "json",
                 contentType: "application/json"
             },
             create: {
                 type: "PUT",
-                url: getBackendURL()+"user",
+                url: getBackendURL() + "user",
                 dataType: "json",
                 contentType: "application/json"
             },
@@ -45,7 +45,10 @@ function initializeUserManagement() {
         pageable: {
             refresh: true
         },
-        toolbar: [{name:"create",text:"Yeni Kullanıcı"}],
+        toolbar: [{
+            name: "create",
+            text: "Yeni Kullanıcı"
+        }],
         columns: [{
             field: "name",
             title: "Ad"
@@ -56,83 +59,99 @@ function initializeUserManagement() {
         }, {
             field: "email",
             title: "E-posta"
-        },{
+        }, {
             field: "password",
             title: "Şifre",
-            hidden:true
+            hidden: true
 
-        },{
-			  field: "roleOid",
-			  title: "Rol",
-              editor: userRoleDropDownEditor,
-              hidden:true
-		},{
-			  field: "firmOid",
-			  title: "Firma",
-			  editor: userFirmDropDownEditor,
-			  hidden:true
-		}, {
+        }, {
+            field: "roleOid",
+            title: "Rol",
+            editor: userRoleDropDownEditor,
+            hidden: true
+        }, {
+            field: "firmOid",
+            title: "Firma",
+            editor: userFirmDropDownEditor,
+            hidden: true
+        }, {
             field: "active",
             title: "Aktif mi?",
             template: "#= (active)? 'Evet':'Hayır'#"
         }, {
-            command: [{name:"edit",text:""},{name: "destroy",text:""}],
+            command: [{
+                name: "edit",
+                text: {
+                    edit: "",
+                    update: "Güncelle",
+                    cancel: "İptal"
+                }
+            }, {
+                name: "destroy",
+                text: ""
+            }],
             title: "&nbsp;",
             width: "130px"
         }],
-        editable: "popup"
+        editable  : {
+		  mode : "popup",
+		  window : {
+			  title: "Kayıt"
+		  }
+	  }
     });
 
     function userRoleDropDownEditor(container, options) {
-    	var cmbDataSource = new kendo.data.DataSource({
-		   transport: {
-			   read: {
-				   type: "GET",
-				   url: getBackendURL()+"role/all",
-				   dataType: "json",
-				   contentType: "application/json"
-			   }
-		   },
-		   schema: {
-			   model: Role
-		   }
-	   });
-		$('<input required  data-text-field="name" data-value-field="oid"  data-bind="value:' + options.field + '"/>')
-			.appendTo(container)
-			.kendoDropDownList({
-				autoBind: false,
-				dataTextField: "name",
+        var cmbDataSource = new kendo.data.DataSource({
+            transport: {
+                read: {
+                    type: "GET",
+                    url: getBackendURL() + "role/all",
+                    dataType: "json",
+                    contentType: "application/json"
+                }
+            },
+            schema: {
+                model: Role
+            }
+        });
+        $('<input required  data-text-field="name" data-value-field="oid"  data-bind="value:' + options.field + '"/>')
+            .appendTo(container)
+            .kendoDropDownList({
+                autoBind: false,
+                dataTextField: "name",
                 dataValueField: "oid",
-				text:"Seçiniz...",
-				dataSource: cmbDataSource ,
-				placeholder: "Seçiniz...",
-				index:-1
-		});
+                text: "Seçiniz...",
+                dataSource: cmbDataSource,
+                placeholder: "Seçiniz...",
+                index: -1
+            });
     }
+
     function userFirmDropDownEditor(container, options) {
-        	var cmbDataSource = new kendo.data.DataSource({
-    		   transport: {
-    			   read: {
-    				   type: "GET",
-    				   url: getBackendURL()+"firm/all",
-    				   dataType: "json",
-    				   contentType: "application/json"
-    			   }
-    		   },
-    		   schema: {
-    			   model: Firm
-    		   }
-    	   });
-    		$('<input required data-bind="value:' + options.field + '"/>')
-    			.appendTo(container)
-    			.kendoDropDownList({
-    				autoBind: false,
-    				dataTextField: "organizationIdentifier",
-                    dataValueField: "oid",
-    				dataSource: cmbDataSource  ,
-    				index:-1 ,
-    				text: "Seçiniz..."
-    		});
-        }
+        var cmbDataSource = new kendo.data.DataSource({
+            transport: {
+                read: {
+                    type: "GET",
+                    url: getBackendURL() + "firm/all",
+                    dataType: "json",
+                    contentType: "application/json"
+                }
+            },
+            schema: {
+                model: Firm
+            }
+        });
+        $('<input required data-bind="value:' + options.field + '"/>')
+            .appendTo(container)
+            .kendoDropDownList({
+                autoBind: false,
+                dataTextField: "organizationIdentifier",
+                dataValueField: "oid",
+                dataSource: cmbDataSource,
+                index: -1,
+                text: "Seçiniz..."
+            });
+    }
 
 }
