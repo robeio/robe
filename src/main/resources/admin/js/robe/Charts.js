c1b1b046e780e6362c7010d6532dc7f8818c1204
Seray Uzgur <seray.uzgur@mebitech.com>
2014-02-03 18:53:48 +0200

    ROBE-23

---
+++ src/main/resources/admin/js/robe/Charts.js
@@ -0,0 +1,133 @@
+var Charts = {
+
+	pie: function(id,data,title){
+		$('#'+id).highcharts({
+			chart: {
+				plotBackgroundColor: null,
+				plotBorderWidth: 0,
+				plotShadow: false
+			},
+			title: {
+				text: title,
+				align: 'center',
+				verticalAlign: 'middle',
+				y: 50
+			},
+			tooltip: {
+				pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+			},
+			plotOptions: {
+				pie: {
+					dataLabels: {
+						enabled: true,
+						distance:5,
+						shadow:true,
+						color:"#333333"
+					},
+					startAngle: -90,
+					endAngle: 90,
+					center: ['50%', '75%'] ,
+					colors:["#00FF00","#FF0000"]
+				}
+			},
+			series: [{
+				type: 'pie',
+				name: 'Oran',
+				innerSize: '50%',
+				data: data
+			}]
+		});
+	} ,
+
+	gauge: function(id,data,title){
+		$('#'+id).highcharts({
+			chart: {
+				type: 'gauge',
+				plotBackgroundColor: null,
+				plotBackgroundImage: null,
+				plotBorderWidth: 0,
+				plotShadow: false
+			},
+
+			title: {
+				text: title
+			},
+			pane: {
+				startAngle: -150,
+				endAngle: 150,
+				background: [{
+					backgroundColor: {
+						linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+						stops: [
+							[0, '#FFF'],
+							[1, '#333']
+						]
+					},
+					borderWidth: 0,
+					outerRadius: '109%'
+				}, {
+					backgroundColor: {
+						linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+						stops: [
+							[0, '#333'],
+							[1, '#FFF']
+						]
+					},
+					borderWidth: 1,
+					outerRadius: '107%'
+				}, {
+					// default background
+				}, {
+					backgroundColor: '#DDD',
+					borderWidth: 0,
+					outerRadius: '105%',
+					innerRadius: '103%'
+				}]
+			},
+
+			yAxis: {
+				min: 0,
+				max: data[1],
+
+				minorTickInterval: 'auto',
+				minorTickWidth: 1,
+				minorTickLength: 10,
+				minorTickPosition: 'inside',
+				minorTickColor: '#666',
+
+				tickPixelInterval: 30,
+				tickWidth: 2,
+				tickPosition: 'inside',
+				tickLength: 10,
+				tickColor: '#666',
+				labels: {
+					step: 2,
+					rotation: 'auto'
+				},
+				title: {
+					text: 'Kullanılan Thread'
+				},
+				plotBands: [{
+					from: 0,
+					to: 120,
+					color: '#55BF3B' // green
+				}, {
+					from: 120,
+					to: 160,
+					color: '#DDDF0D' // yellow
+				}, {
+					from: 160,
+					to: 200,
+					color: '#DF5353' // red
+				}]
+			},
+
+			series: [{
+				name: 'Bağlantı',
+				data: [data[0]]
+			}]
+
+
+		});
+	}
+}
