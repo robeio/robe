c25876237d1835cfe24adff35493a2e5afe7c9e8
Seray Uzgur <seray.uzgur@mebitech.com>
2014-02-06 10:58:01 +0200

    ROBE-33

--- src/main/java/io/robe/resources/RoleResource.java
+++ src/main/java/io/robe/resources/RoleResource.java
@@ -21,54 +21,54 @@ import java.util.List;
 public class RoleResource {
 
 
-	@Inject
-	RoleDao roleDao;
+    @Inject
+    RoleDao roleDao;
 
-	@Path("all")
-	@GET
-	@UnitOfWork
-	public List<Role> getRoles(@Auth Credentials credentials) {
+    @Path("all")
+    @GET
+    @UnitOfWork
+    public List<Role> getRoles(@Auth Credentials credentials) {
 
-		return roleDao.findAll(Role.class);
-	}
+        return roleDao.findAll(Role.class);
+    }
 
-	@GET
-	@UnitOfWork
-	@Path("{userId}")
-	public Role get(@Auth Credentials credentials, @PathParam("userId") String id) {
-		Role role = roleDao.findById(id);
-		Hibernate.initialize(role.getRoles());
-		return role;
-	}
+    @GET
+    @UnitOfWork
+    @Path("{userId}")
+    public Role get(@Auth Credentials credentials, @PathParam("userId") String id) {
+        Role role = roleDao.findById(id);
+        Hibernate.initialize(role.getRoles());
+        return role;
+    }
 
-	@PUT
-	@UnitOfWork
-	public Role create(@Auth Credentials credentials, @Valid Role role) {
-		Optional<Role> checkUser = roleDao.findByName(role.getCode());
-		if (checkUser.isPresent())
-			throw new RobeRuntimeException("Code", role.getCode() + " already used by another role. Please use different code.");
-		role = roleDao.create(role);
-		return role;
-	}
+    @PUT
+    @UnitOfWork
+    public Role create(@Auth Credentials credentials, @Valid Role role) {
+        Optional<Role> checkUser = roleDao.findByName(role.getCode());
+        if (checkUser.isPresent())
+            throw new RobeRuntimeException("Code", role.getCode() + " already used by another role. Please use different code.");
+        role = roleDao.create(role);
+        return role;
+    }
 
-	@POST
-	@UnitOfWork
-	public Role update(@Auth Credentials credentials, Role role) {
-		roleDao.detach(role);
-		Role entity = roleDao.findById(role.getOid());
-		entity.setName(role.getName());
-		entity.setCode(role.getCode());
-		entity = roleDao.update(entity);
-		return entity;
+    @POST
+    @UnitOfWork
+    public Role update(@Auth Credentials credentials, Role role) {
+        roleDao.detach(role);
+        Role entity = roleDao.findById(role.getOid());
+        entity.setName(role.getName());
+        entity.setCode(role.getCode());
+        entity = roleDao.update(entity);
+        return entity;
 
-	}
+    }
 
 
-	@DELETE
-	@UnitOfWork
-	public Role delete(@Auth Credentials credentials, Role role) {
-		role = roleDao.findById(role.getOid());
-		roleDao.delete(role);
-		return role;
-	}
+    @DELETE
+    @UnitOfWork
+    public Role delete(@Auth Credentials credentials, Role role) {
+        role = roleDao.findById(role.getOid());
+        roleDao.delete(role);
+        return role;
+    }
 }
