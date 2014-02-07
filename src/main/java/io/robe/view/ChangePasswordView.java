70f1a443543c2b5f727a405ff6768f7d0b17c90e
kaan.alkim <kaan.alkim@mebitech.com>
2014-02-07 12:04:03 +0200

    change pass

--- src/main/java/io/robe/view/ChangePasswordView.java
+++ src/main/java/io/robe/view/ChangePasswordView.java
@@ -5,8 +5,15 @@ import com.yammer.dropwizard.views.View;
 
 public class ChangePasswordView extends View {
 
+    private String tickedOid;
+
     public ChangePasswordView(String tickedOid) {
         super("ChangePassword.ftl");
+        this.tickedOid = tickedOid;
+    }
+
+    public String getTickedOid() {
+        return this.tickedOid;
     }
 
 }
