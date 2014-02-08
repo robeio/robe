4f4f0bd63a6fb4cedd25f7ddf552a8bd2f00c59c
kaan.alkim <kaan.alkim@mebitech.com>
2014-02-08 17:10:56 +0200

    ROBE-52 logback charts

--- src/main/resources/admin/js/robe/Dashboard.js
+++ src/main/resources/admin/js/robe/Dashboard.js
@@ -7,27 +7,46 @@ function initializeDashboard() {
         }
     })
     $.ajax({
-    		type: "GET",
-    		url: "http://127.0.0.1:8080/admin/metrics",
-    		dataType: "json",
-    		crossDomain: true,
+        type: "GET",
+        url: "http://127.0.0.1:8080/admin/metrics",
+        dataType: "json",
+        crossDomain: true,
 
-    		contentType: "application/json; charset=utf-8",
-    		success: function(response){
-    			var data = response["jvm"]["memory"];
-				var memoryData = [["Boş",(data["totalMax"]-data["totalUsed"])],["Kullanılan",(data["totalUsed"])]];
+        contentType: "application/json; charset=utf-8",
+        success: function (response) {
+            var data = response["jvm"]["memory"];
+            var memoryData = [
+                ["Boş", (data["totalMax"] - data["totalUsed"])],
+                ["Kullanılan", (data["totalUsed"])]
+            ];
 
-				var connPool = response["org.eclipse.jetty.util.thread.QueuedThreadPool"];
-				var connMax = connPool["active-threads"]["value"] + connPool["idle-threads"]["value"];
-				var connUsed = connPool["active-threads"]["value"];
-				Charts.pie("memory",memoryData,"Ram");
-				Charts.gauge("memory2",[connUsed,connMax],"Threads");
-    		},
-    		error: function(jqXHR, textStatus, errorThrown){
-                    console.log(arguments);
-                    alert('HTTP Error: '+errorThrown+' | Error Message: '+textStatus);
-                    return;
-                }
+            var connPool = response["org.eclipse.jetty.util.thread.QueuedThreadPool"];
+            var connMax = connPool["active-threads"]["value"] + connPool["idle-threads"]["value"];
+            var connUsed = connPool["active-threads"]["value"];
+
+            var logbackAll = response["ch.qos.logback.core.Appender"]["all"]["count"];
+            var logbackDebug = response["ch.qos.logback.core.Appender"]["debug"]["count"];
+            var logbackError = response["ch.qos.logback.core.Appender"]["error"]["count"];
+            var logbackInfo = response["ch.qos.logback.core.Appender"]["info"]["count"];
+            var logbackTrace = response["ch.qos.logback.core.Appender"]["trace"]["count"];
+            var logbackWarn = response["ch.qos.logback.core.Appender"]["warn"]["count"];
+            var logBackData = [
+                ["Debug", (logbackDebug)],
+                ["Error", (logbackError)],
+                ["Info", (logbackInfo)],
+                ["Trace", (logbackTrace)],
+                ["Warn", (logbackWarn)]
+            ];
+
+            Charts.pie("memory", memoryData, "Ram");
+            Charts.gauge("threads", [connUsed, connMax], "Threads");
+            Charts.pie("allLogback", logBackData, "LOGBACK")
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(arguments);
+            alert('HTTP Error: ' + errorThrown + ' | Error Message: ' + textStatus);
+            return;
+        }
 
     });
 
