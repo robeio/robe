f7228ec88db116354f555488f9dcce9db0540481
Seray Uzgur <seray.uzgur@mebitech.com>
2014-02-10 15:15:36 +0200

    ROBE-42

--- src/main/java/io/robe/resources/UserResource.java
+++ src/main/java/io/robe/resources/UserResource.java
@@ -8,13 +8,20 @@ import io.robe.auth.Credentials;
 import io.robe.dto.UserDTO;
 import io.robe.exception.RobeRuntimeException;
 import io.robe.hibernate.dao.RoleDao;
+import io.robe.hibernate.dao.TicketDao;
 import io.robe.hibernate.dao.UserDao;
 import io.robe.hibernate.entity.Role;
+import io.robe.hibernate.entity.Ticket;
 import io.robe.hibernate.entity.User;
+import io.robe.mail.MailSender;
 import io.robe.utils.HashingUtils;
+import org.joda.time.DateTime;
 
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.util.LinkedList;
 import java.util.List;
@@ -25,30 +32,32 @@ import java.util.List;
 public class UserResource {
 
 
-	@Inject
-	UserDao userDao;
-	@Inject
-	RoleDao roleDao;
+    @Inject
+    UserDao userDao;
+    @Inject
+    RoleDao roleDao;
+    @Inject
+    TicketDao ticketDao;
 
-	@Path("all")
-	@GET
-	@UnitOfWork
-	public List<UserDTO> getUsers(@Auth Credentials credentials) {
-		List<io.robe.hibernate.entity.User> entities = userDao.findAll(io.robe.hibernate.entity.User.class);
-		List<UserDTO> users = new LinkedList<UserDTO>();
-		for (io.robe.hibernate.entity.User entity : entities) {
-			UserDTO user = new UserDTO(entity);
-			users.add(user);
-		}
-		return users;
-	}
+    @Path("all")
+    @GET
+    @UnitOfWork
+    public List<UserDTO> getUsers(@Auth Credentials credentials) {
+        List<io.robe.hibernate.entity.User> entities = userDao.findAll(io.robe.hibernate.entity.User.class);
+        List<UserDTO> users = new LinkedList<UserDTO>();
+        for (io.robe.hibernate.entity.User entity : entities) {
+            UserDTO user = new UserDTO(entity);
+            users.add(user);
+        }
+        return users;
+    }
 
-	@GET
-	@UnitOfWork
-	@Path("{userId}")
-	public UserDTO get(@Auth Credentials credentials, @PathParam("userId") String id) {
-		return new UserDTO(userDao.findById(id));
-	}
+    @GET
+    @UnitOfWork
+    @Path("{userId}")
+    public UserDTO get(@Auth Credentials credentials, @PathParam("userId") String id) {
+        return new UserDTO(userDao.findById(id));
+    }
 
     @GET
     @UnitOfWork
@@ -58,63 +67,79 @@ public class UserResource {
     }
 
     @PUT
-	@UnitOfWork
-	public UserDTO create(@Auth Credentials credentials, @Valid UserDTO user) {
-		Optional<User> checkUser = userDao.findByEmail(user.getEmail());
-		if (checkUser.isPresent())
-			throw new RobeRuntimeException("E-mail", user.getEmail() + " already used by another user. Please use different e-mail.");
-		User entity = new User();
-		entity.setEmail(user.getEmail());
-		entity.setName(user.getName());
-		entity.setSurname(user.getSurname());
-		entity.setPassword(user.getPassword());
-		entity.setActive(user.isActive());
-		Role role = roleDao.findById(user.getRoleOid());
-		if (role == null) {
-			throw new RobeRuntimeException("Role", user.getEmail() + " cannot be null.");
-		}
-		entity.setRole(role);
-		entity.setPassword(HashingUtils.hashSHA2(user.getPassword()));
+    @UnitOfWork
+    public UserDTO create(@Auth Credentials credentials, @Valid UserDTO user, @Context HttpServletRequest request) {
+        Optional<User> checkUser = userDao.findByEmail(user.getEmail());
+        if (checkUser.isPresent())
+            throw new RobeRuntimeException("E-mail", user.getEmail() + " already used by another user. Please use different e-mail.");
+        User entity = new User();
+        entity.setEmail(user.getEmail());
+        entity.setName(user.getName());
+        entity.setSurname(user.getSurname());
+        entity.setActive(false);
+        Role role = roleDao.findById(user.getRoleOid());
+        if (role == null) {
+            throw new RobeRuntimeException("Role", user.getEmail() + " cannot be null.");
+        }
+        entity.setRole(role);
+        entity.setPassword(HashingUtils.hashSHA2(user.getPassword()));
 
-		return new UserDTO(userDao.create(entity));
+        if (MailSender.isSupported()) {
+            Ticket ticket = new Ticket();
+            ticket.setType(Ticket.Type.ACTIVATE);
+            DateTime expire = DateTime.now().plusDays(1);
+            ticket.setExpirationDate(expire.toDate());
+            ticket.setUser(entity);
+            ticketDao.create(ticket);
+            try {
+                //TODO: Template support will be used.
+                MailSender.send("serayuzgur@gmail.com", new String[]{entity.getEmail()}, "Activation", ticket.getOid(), null);
+            } catch (MessagingException e) {
+                new RobeRuntimeException(e);
+            }
+        } else {
+            entity.setActive(true);
+        }
 
-	}
+        return new UserDTO(userDao.create(entity));
 
-	@POST
-	@UnitOfWork
-	public UserDTO update(@Auth Credentials credentials, @Valid UserDTO user) {
-		// Get and check user
-		User entity = userDao.findById(user.getOid());
-		if (entity == null)
-			throw new RobeRuntimeException("User", user.getOid() + " not exists.");
-		//Get role and firm and check for null
-		Role role = roleDao.findById(user.getRoleOid());
-		if (role == null) {
-			throw new RobeRuntimeException("Role", user.getEmail() + " cannot be null.");
-		}
-		entity.setOid(user.getOid());
-		entity.setEmail(user.getEmail());
-		entity.setName(user.getName());
-		entity.setSurname(user.getSurname());
-		entity.setPassword(user.getPassword());
-		entity.setActive(user.isActive());
-		entity.setRole(role);
-		entity.setPassword(HashingUtils.hashSHA2(user.getPassword()));
+    }
 
-		entity = userDao.update(entity);
-		userDao.flush();
+    @POST
+    @UnitOfWork
+    public UserDTO update(@Auth Credentials credentials, @Valid UserDTO user) {
+        // Get and check user
+        User entity = userDao.findById(user.getOid());
+        if (entity == null)
+            throw new RobeRuntimeException("User", user.getOid() + " not exists.");
+        //Get role and firm and check for null
+        Role role = roleDao.findById(user.getRoleOid());
+        if (role == null) {
+            throw new RobeRuntimeException("Role", user.getEmail() + " cannot be null.");
+        }
+        entity.setOid(user.getOid());
+        entity.setEmail(user.getEmail());
+        entity.setName(user.getName());
+        entity.setSurname(user.getSurname());
+        entity.setPassword(user.getPassword());
+        entity.setActive(user.isActive());
+        entity.setRole(role);
+        entity.setPassword(HashingUtils.hashSHA2(user.getPassword()));
 
-		return new UserDTO(entity);
+        entity = userDao.update(entity);
+        userDao.flush();
 
-	}
+        return new UserDTO(entity);
 
-	@DELETE
-	@UnitOfWork
-	public UserDTO delete(@Auth Credentials credentials, UserDTO user) {
-		User entity = userDao.findById(user.getOid());
+    }
 
-		userDao.delete(entity);
-		return user;
-	}
+    @DELETE
+    @UnitOfWork
+    public UserDTO delete(@Auth Credentials credentials, UserDTO user) {
+        User entity = userDao.findById(user.getOid());
+
+        userDao.delete(entity);
+        return user;
+    }
 
 }
