71a93d648aa770f1db7d2e8f59e7ded3f1393d23
kaan.alkim <kaan.alkim@mebitech.com>
2014-02-11 10:10:11 +0200

    ROBE-53

--- src/main/resources/admin/js/robe/UserManagement.js
+++ src/main/resources/admin/js/robe/UserManagement.js
@@ -3,51 +3,58 @@ function initializeUserManagement() {
     $("#gridUsers").kendoGrid({
         dataSource: UserDataSource,
         sortable: true,
-        toolbar: [{
-            name: "create",
-            text: "Yeni Kullanıcı"
-        }],
-        columns: [{
-            field: "name",
-            title: "Ad"
+        toolbar: [
+            {
+                name: "create",
+                text: "Yeni Kullanıcı"
+            }
+        ],
+        columns: [
+            {
+                field: "name",
+                title: "Ad"
 
-        }, {
-            field: "surname",
-            title: "Soyad"
-        }, {
-            field: "email",
-            title: "E-posta"
-        }, {
-            field: "password",
-            title: "Şifre",
-            hidden: true
-
-        }, {
-            field: "roleOid",
-            title: "Rol",
-            editor: userRoleDropDownEditor,
-            hidden: true
-        },  {
-            field: "active",
-            title: "Aktif mi?",
-            template: "#= (active)? 'Evet':'Hayır'#"
-        }, {
-            command: [{
-                name: "edit",
-                text: {
-                    edit: "",
-                    update: "Güncelle",
-                    cancel: "İptal"
-                },
-                className:"grid-command-iconfix"
-            }, {
-                name: "destroy",
-                text: "" ,
-                className:"grid-command-iconfix"
-            }],
-            title: "&nbsp;",
-            width: "80px"
-        }],
+            },
+            {
+                field: "surname",
+                title: "Soyad"
+            },
+            {
+                field: "email",
+                title: "E-posta"
+            },
+            {
+                field: "roleOid",
+                title: "Rol",
+                editor: userRoleDropDownEditor,
+                hidden: true
+            },
+            {
+                field: "active",
+                title: "Aktif mi?",
+                template: "#= (active)? 'Evet':'Hayır'#"
+            },
+            {
+                command: [
+                    {
+                        name: "edit",
+                        text: {
+                            edit: "",
+                            update: "Güncelle",
+                            cancel: "İptal"
+                        },
+                        className: "grid-command-iconfix"
+                    },
+                    {
+                        name: "destroy",
+                        text: "",
+                        className: "grid-command-iconfix"
+                    }
+                ],
+                title: "&nbsp;",
+                width: "80px"
+            }
+        ],
         editable: {
             mode: "popup",
             window: {
@@ -76,16 +83,16 @@ function initializeUserManagement() {
         click: onShowHelp
     });
 
-    function onShowHelp () {
+    function onShowHelp() {
         wnd = $("#userManagementHelpWindow").kendoWindow({
             title: "Yardım",
             modal: true,
             visible: false,
             resizable: false,
             width: 500
-            }).data("kendoWindow");
+        }).data("kendoWindow");
 
-            wnd.center().open();
+        wnd.center().open();
 
     };
 }
