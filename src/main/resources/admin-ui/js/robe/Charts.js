9bc40a05769925c79d1b2aebacbcfd671c0d2319
Seray Uzgur <seray.uzgur@mebitech.com>
2014-02-11 16:30:53 +0200

    ROBE-36

---
+++ src/main/resources/admin-ui/js/robe/Charts.js
@@ -0,0 +1,187 @@
+//@ sourceURL=Charts.js
+var Charts = {
+
+	pie: function(id,data,title){
+		$('#'+id).highcharts({
+			chart: {
+				plotBackgroundColor: null,
+				plotBorderWidth: 0,
+				plotShadow: false
+			},
+			title: {
+				text: title,
+				align: 'center',
+				verticalAlign: 'middle',
+				y: 50
+			},
+			tooltip: {
+				pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b> ({point.y})Bytes'
+			},
+			plotOptions: {
+				pie: {
+					dataLabels: {
+						enabled: true,
+						distance:5,
+						shadow:true,
+						color:"#333333"
+					},
+					startAngle: -90,
+					endAngle: 90,
+					center: ['50%', '75%'] ,
+                    colors: ["#00FF00", "#FF0000", "#0000FF"]
+                }
+			},
+			series: [{
+				type: 'pie',
+				name: 'Oran',
+				innerSize: '50%',
+				data: data
+			}]
+		});
+	},
+	column : function(id,data,title,serverUptime) {
+	$(function () {
+            $('#'+id).highcharts({
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: title
+                },
+                subtitle: {
+                    text: serverUptime
+                },
+                xAxis: {
+                    categories: [
+                        '1**',
+                        '2**',
+                        '3**',
+                        '4**',
+                        '5**'
+                    ]
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Count'
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+                series: [{
+                    name: 'Http Response Code',
+                    data: data,
+
+                }]
+            });
+        });
+
+
+
+	},
+
+
+	gauge: function(id,data,title){
+		$('#'+id).highcharts({
+			chart: {
+				type: 'gauge',
+				plotBackgroundColor: null,
+				plotBackgroundImage: null,
+				plotBorderWidth: 0,
+				plotShadow: false
+			},
+
+			title: {
+				text: title
+			},
+			pane: {
+				startAngle: -150,
+				endAngle: 150,
+				background: [{
+					backgroundColor: {
+						linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+						stops: [
+							[0, '#FFF'],
+							[1, '#333']
+						]
+					},
+					borderWidth: 0,
+					outerRadius: '109%'
+				}, {
+					backgroundColor: {
+						linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+						stops: [
+							[0, '#333'],
+							[1, '#FFF']
+						]
+					},
+					borderWidth: 1,
+					outerRadius: '107%'
+				}, {
+					// default background
+				}, {
+					backgroundColor: '#DDD',
+					borderWidth: 0,
+					outerRadius: '105%',
+					innerRadius: '103%'
+				}]
+			},
+
+			yAxis: {
+				min: 0,
+				max: data[1],
+
+				minorTickInterval: 'auto',
+				minorTickWidth: 1,
+				minorTickLength: 5,
+				minorTickPosition: 'inside',
+				minorTickColor: '#666',
+
+				tickPixelInterval: 30,
+				tickWidth: 2,
+				tickPosition: 'inside',
+				tickLength: 10,
+				tickColor: '#666',
+				labels: {
+					step: 2,
+					rotation: 'auto'
+				},
+				title: {
+					text: 'Active Thread'
+				},
+				plotBands: [{
+					from: 0,
+					to: (data[1]*0.7).toFixed(0),
+					color: '#55BF3B' // green
+				}, {
+					from: (data[1]*0.7).toFixed(0),
+					to: (data[1]*0.85).toFixed(0),
+					color: '#DDDF0D' // yellow
+				}, {
+					from: (data[1]*0.85).toFixed(0),
+					to: data[1],
+					color: '#DF5353' // red
+				}]
+			},
+
+			series: [{
+				name: 'Active',
+				data: [data[0]]
+			}]
+
+
+		});
+	}
+}
---
+++ src/main/resources/admin-ui/js/robe/Workspace.js
@@ -0,0 +1,192 @@
+//@ sourceURL=Workspace.js
+$.ajaxSetup({
+    dataType: "json",
+    statusCode: {
+        401: function () {
+            loadLogin();
+        },
+        422: function (xhr) {
+            var errors = JSON.parse(xhr.responseText);
+            var msg = "<span style='float:right;'>";
+            $.each(errors, function (index, error) {
+                if (error.name != "") {
+                    msg += "<br> <b>" + error.name
+                    if (error.message != "")
+                        msg += ":";
+                    msg += "</b> "
+                }
+                if (error.message != "") {
+                    msg += error.message;
+                }
+            });
+            showDialog("<img style='float:left; margin-right:10px;' src='./icon/close.png'/>" + msg + "</span>", "Hata");
+        }
+    },
+    beforeSend: function (xhr) {
+
+    },
+    complete: function (xhr, status) {
+        console.log(status + " : " + xhr);
+    }
+});
+loadConfig();
+$(document).ready(function () {
+
+    $("#progressBar").kendoProgressBar({
+        min: 0,
+        max: 1,
+        type: "value",
+        showStatus: false,
+        animation: {
+            duration: 200
+        }
+    });
+
+    $("#profile").click(function () {
+        $('#dialogMessage').load("./ProfileManagement.html", function () {
+            showDialog(null, "Profil Bilgileri");
+            eval("initializeProfileManagement();");
+        });
+    });
+
+    $("#logout").click(function () {
+        $.cookie.destroy("MedyAuthToken");
+        location.reload();
+    });
+
+    $("#settings").kendoButton({
+        click: onClickSettingsButton
+    });
+
+    function onClickSettingsButton(e) {
+        $("#dropdownMenu").toggle("slow");
+    }
+
+    $('#dialog').kendoWindow({
+        actions: ["Close"],
+        modal: true,
+        visible: false
+    });
+
+
+    loadLogin();
+
+    $(document).ajaxStart(function () {
+        showIndicator(true);
+    });
+    $(document).ajaxStop(function () {
+        showIndicator(false);
+    });
+});
+
+function loadMenu() {
+    $.ajax({
+        type: "GET",
+        url: getBackendURL() + "menu/user",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (response) {
+            addIcons(response[0]);
+            $('#menu').kendoPanelBar({
+                dataSource: response[0].items,
+                select: onSelect
+            });
+
+        }
+    });
+}
+
+function addIcons(menu) {
+    if (menu.hasOwnProperty("items")) {
+        for (var i = 0; i < menu.items.length; i++) {
+            addIcons(menu.items[i]);
+        }
+    }
+    menu.imageUrl = "../icon/menu/" + menu.cssClass.substring(8) + ".png";
+}
+
+function onSelect(e) {
+
+    var selection = "k-";
+    for (var i = 0; i < e.item.classList.length; i++) {
+        var css = e.item.classList[i];
+        if (css.indexOf("command:") == 0) {
+            selection = css.substring(8);
+            break;
+        }
+    }
+    openMenuItem(selection);
+}
+//
+function openMenuItem(menuitem) {
+
+    kendo.destroy($('#container'));
+    $('#container').html('');
+
+    if (menuitem.indexOf("k-") == 0)
+        return;
+
+    $('#container').load("./" + menuitem + ".html", function () {
+        try {
+            $.getScript("../js/robe/" + menuitem + ".js", function () {
+                try {
+                    eval("initialize" + menuitem + "();");
+                } catch (e) {
+                    console.log(menuitem + " JS: " + e);
+                }
+            });
+        } catch (e) {
+            console.log(menuitem + " HTML: " + e);
+        }
+        kendo.fx($("#container")).fade("in").play();
+    });
+}
+//
+function showIndicator(show) {
+    if (show)
+        $("#progressBar").data("kendoProgressBar").value(0);
+    else
+        $("#progressBar").data("kendoProgressBar").value(1);
+
+}
+
+function showDialog(message, title) {
+    if (message != null)
+        $('#dialogMessage').html(message);
+    if (title == null)
+        title = "";
+    $('#dialog').data("kendoWindow").title(title);
+    $('#dialog').data("kendoWindow").center();
+    $('#dialog').data("kendoWindow").open();
+}
+
+function loadLogin() {
+    $('#dialogMessage').load("./Login.html", function () {
+        showDialog(null, "Giriş");
+        $.getScript("../js/robe/Login.js", function () {
+            eval("initializeLogin();");
+        });
+
+    });
+}
+
+function loadConfig() {
+    $.ajax({
+        dataType: "json",
+        url: "../config.json",
+        success: function (response) {
+            backendURL = response.backendURL;
+            adminURL = response.adminURL;
+        }
+    });
+}
+var backendURL = "../../robe/";
+var adminURL = "";
+
+function getBackendURL() {
+    return backendURL;
+}
+
+function getAdminURL() {
+    return adminURL;
+}
