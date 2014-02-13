6cf9cdc06c82d516fcf1dc345c1f0981770fe1fe
kaan.alkim <kaan.alkim@mebitech.com>
2014-02-13 17:35:58 +0200

    ROBE-55

--- src/main/java/io/robe/resources/MailTemplateResource.java
+++ src/main/java/io/robe/resources/MailTemplateResource.java
@@ -4,18 +4,14 @@ import com.google.inject.Inject;
 import com.yammer.dropwizard.auth.Auth;
 import com.yammer.dropwizard.hibernate.UnitOfWork;
 import io.robe.auth.Credentials;
-import io.robe.dto.BasicPair;
 import io.robe.hibernate.dao.MailTemplateDao;
 import io.robe.hibernate.entity.MailTemplate;
 
-import javax.sql.rowset.serial.SerialBlob;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-import java.sql.Blob;
-import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.List;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -29,33 +25,55 @@ public class MailTemplateResource {
     @Inject
     MailTemplateDao mailTemplateDao;
 
+    @GET
+    @Path("/all")
+    @UnitOfWork
+    public List<MailTemplate> getAll() {
+        List<MailTemplate> list = mailTemplateDao.findAll(MailTemplate.class);
+        return list;
+    }
+
     @PUT
     @UnitOfWork
-    public HashMap createTemplate(@Auth Credentials credentials, HashMap<String, String> data) {
-        String tLang = data.get("tLang");
+    public MailTemplate createTemplate(@Auth Credentials credentials, HashMap<String, String> data) {
+        String language = data.get("lang");
+        String code = data.get("code");
         String template = data.get("template");
 
-        checkNotNull(tLang, "tLang mustn't be null or empty");
+        checkNotNull(language, "tLang mustn't be null or empty");
         checkNotNull(template, "template mustn't be null or empty");
+        checkNotNull(code, "tCode mustn't be null or empty");
 
-        MailTemplate mailTemplate = new MailTemplate();
-        if (tLang.equals("tr")) {
-            mailTemplate.setLang(MailTemplate.Type.TR);
-        } else if (tLang.equals("en")) {
-            mailTemplate.setLang(MailTemplate.Type.EN);
+        MailTemplate entity = new MailTemplate();
+        if (language.equals("TR")) {
+            entity.setLang(MailTemplate.Type.TR);
+        } else if (language.equals("EN")) {
+            entity.setLang(MailTemplate.Type.EN);
         }
 
-        byte[] byteTemplate = template.getBytes();
-        try {
-            Blob blobTemplate = new SerialBlob(byteTemplate);
-            mailTemplate.setTemplate(blobTemplate);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        entity.setCode(code);
+        entity.setTemplate(template);
+        mailTemplateDao.create(entity);
 
-        mailTemplateDao.create(mailTemplate);
-
-        return data;
+        return entity;
     }
+
+//    @POST
+//    @UnitOfWork
+//    public HashMap updateTemplate(@Auth Credentials credentials, HashMap<String, String> data) {
+//        String tLang = data.get("tLang");
+//        String template = data.get("template");
+//        String tCode = data.get("tCode");
+//
+//        checkNotNull(tLang, "tLang mustn't be null or empty");
+//        checkNotNull(template, "template mustn't be null or empty");
+//        checkNotNull(tCode, "tCode mustn't be null or empty");
+//
+//        MailTemplate mailTemplate = new MailTemplate();
+//
+//
+//
+//
+//    }
 
 }
