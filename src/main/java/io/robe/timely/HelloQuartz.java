1fd4e1389d97f331191433abb4e8389e4b360ed4
Seray Uzgur <seray.uzgur@mebitech.com>
2014-02-18 14:08:44 +0200

    ROBE-36

--- src/main/java/io/robe/timely/HelloQuartz.java
+++ src/main/java/io/robe/timely/HelloQuartz.java
@@ -1,21 +1,14 @@
 package io.robe.timely;
 
-import io.robe.mail.MailSender;
 import org.quartz.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.mail.MessagingException;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by sinanselimoglu on 14/02/14.
  */
 
-@Timely(cron="0 0/33 * * * ?")
+@Scheduled(cron="0 0/33 * * * ?",manager = Scheduled.Manager.ANNOTATION)
 public class HelloQuartz implements Job {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
     public HelloQuartz() {
