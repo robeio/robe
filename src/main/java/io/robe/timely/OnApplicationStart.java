c48561606c1a715ed90a2cdf8d1472c1cee2d0c1
Seray Uzgur <seray.uzgur@mebitech.com>
2014-02-18 14:37:50 +0200

    ROBE-38

---
+++ src/main/java/io/robe/timely/OnApplicationStart.java
@@ -0,0 +1,15 @@
+package io.robe.timely;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation class for cron operations operations to run at application start.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OnApplicationStart {
+
+}
