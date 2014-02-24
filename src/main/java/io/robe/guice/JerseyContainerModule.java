682caa8bc80e49831371eab7a27848650749e6c1
Seray Uzgur <seray.uzgur@mebitech.com>
2014-02-24 15:17:47 +0200

    ROBE-61

---
+++ src/main/java/io/robe/guice/JerseyContainerModule.java
@@ -0,0 +1,24 @@
+package io.robe.guice;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.sun.jersey.spi.container.WebApplication;
+
+public class JerseyContainerModule extends AbstractModule {
+
+    private final GuiceContainer container;
+
+    public JerseyContainerModule(final GuiceContainer container) {
+        this.container = container;
+    }
+
+    @Override
+    protected void configure() {
+        bind(GuiceContainer.class).toInstance(container);
+    }
+
+    @Provides
+    public WebApplication webApp() {
+        return container.getWebApplication();
+    }
+}
