2b8ea92ff1614eed386b37821bd55d74c748cb13
Seray Uzgur <seray.uzgur@mebitech.com>
2014-03-07 21:08:14 +0200

    ROBE-34

--- robe-admin/src/main/resources/admin-ui/js/main.js
+++ robe-admin/src/main/resources/admin-ui/js/main.js
@@ -2,36 +2,46 @@
 // There usage will become more apparent further along in the tutorial.
 require.config({
     paths: {
-        jquery: 'bower_components/jquery/dist/jquery.min',
-        underscore: 'bower_components/underscore/underscore',
-        router: 'bower_components/requirejs-router/router',
-        myrouter: "router",
-        backbone: 'bower_components/backbone/backbone',
         text: 'require/text',
         html: '../html',
         view: 'admin/view',
-        kendo: 'kendoui/js',
-        datasources: 'admin/data'
+        kendo: 'kendoui/js'
     },
     shim: {
+        'bower_components/jquery/dist/jquery.min': {
+            exports: '$'
+        },
+        'bower_components/underscore/underscore': {
+            exports: '_'
+        },
+        'bower_components/backbone/backbone': {
+            //These script dependencies should be loaded before loading
+            //backbone.js
+            deps: ['bower_components/underscore/underscore', 'bower_components/jquery/dist/jquery.min'],
+            //Once loaded, use the global 'Backbone' as the
+            //module value.
+            exports: 'Backbone'
+        },
         'cryptojs/enc-base64-min': {
             deps: ['cryptojs/core-min']
         },
         'cryptojs/sha256': {
             deps: ['cryptojs/enc-base64-min']
         },
-        'robe/core/Singleton':{
-            deps:['robe/core/Class']
+        'robe/core/Singleton': {
+            deps: ['robe/core/Class']
         }
 
     }
 });
 
 // Load our app module and pass it to our definition function
-require([
+define([
     'admin/AdminUIApp',
-    'robe/core/Class',
-    'robe/core/Singleton'], function (App) {
+    'bower_components/jquery/dist/jquery.min',
+    'bower_components/underscore/underscore',
+    'bower_components/backbone/backbone'
+], function (App) {
     App.initialize();
 });
 
