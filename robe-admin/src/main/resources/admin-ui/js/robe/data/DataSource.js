65beb30b1f53c51742e26fe04aab78d194d6784c
kaan.alkim <kaan.alkim@mebitech.com>
2014-03-14 17:53:46 +0200

    ROBE-79 added initialize into DataSources

--- robe-admin/src/main/resources/admin-ui/js/robe/data/DataSource.js
+++ robe-admin/src/main/resources/admin-ui/js/robe/data/DataSource.js
@@ -3,10 +3,6 @@ function Datasource() {
     this.name;
 
     Robe.call(this, "Datasource");
-
-    Datasource.prototype.getName = function () {
-        return this.name + " kaan";
-    };
 };
 
 Datasource.prototype = new Robe();
