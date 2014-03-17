f09ecc833d2ec2790e9344ad4545c3f001f3affe
kaan.alkim <kaan.alkim@mebitech.com>
2014-03-17 15:00:10 +0200

    #ROBE-79 new robe view class

--- robe-admin/src/main/resources/admin-ui/js/robe/view/Page.js
+++ robe-admin/src/main/resources/admin-ui/js/robe/view/Page.js
@@ -1,33 +1,69 @@
 //@ sourceURL=robe/view/Page.js
-robe.view = robe.view || {};
+//robe.view = robe.view || {};
+//
+//robe.view.Page = robe.view.Page || {};
+//
+//
+//robe.view.Page = {
+//    containerId: null,
+//    htmlPath: null,
+//    setContainerId: function (containerId) {
+//        this.containerId = containerId;
+//    },
+//    bindHtml: function (path) {
+//        this.htmlPath = path
+//    },
+//
+//    show: function (preAction,postAction) {
+//        if(preAction)
+//            preAction();
+//
+//        var me = this;
+//        $("#" + this.containerId).load(this.htmlPath, function () {
+//            me.initialize();
+//            if(postAction)
+//                postAction();
+//        });
+//    },
+//    initialize: function () {
+//        console.warn("Child class must implement function.");
+//    }
+//}
 
-robe.view.Page = robe.view.Page || {};
+var RobeView = (function () {
+    this.name = null;
+    this.containerId = null;
+    this.htmlPath = null;
 
-
-robe.view.Page = {
-    containerId: null,
-    htmlPath: null,
-    setContainerId: function (containerId) {
+    function RobeView(name, htmlPath, containerId) {
+        this.name = name;
+        this.htmlPath = htmlPath;
         this.containerId = containerId;
-    },
-    bindHtml: function (path) {
-        this.htmlPath = path
-    },
+    }
 
-    show: function (preAction,postAction) {
-        if(preAction)
+    RobeView.prototype.show = function (preAction, postAction) {
+        if (preAction) {
             preAction();
+        }
 
         var me = this;
+
         $("#" + this.containerId).load(this.htmlPath, function () {
             me.initialize();
-            if(postAction)
+            if (postAction) {
                 postAction();
+            }
         });
-    },
-    initialize: function () {
-        console.warn("Child class must implement function.");
-    }
-}
+    };
 
+    RobeView.prototype.render = function () {
+        console.warn(this.name + " render run");
+    }
+
+    RobeView.prototype.initialize = function () {
+        console.warn(this.name + " initialize run");
+    }
+
+    return RobeView;
+})();
 
