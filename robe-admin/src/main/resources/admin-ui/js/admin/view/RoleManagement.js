4d65e1e234351476e958a1c5e4a49b96f0a69887
kaan.alkim <kaan.alkim@mebitech.com>
2014-03-17 15:21:21 +0200

    #ROBE-79 RoleManagement modified according to RobeView

--- robe-admin/src/main/resources/admin-ui/js/admin/view/RoleManagement.js
+++ robe-admin/src/main/resources/admin-ui/js/admin/view/RoleManagement.js
@@ -1,4 +1,5 @@
 //@ sourceURL=RoleManagement.js
+var RoleManagementView;
 define([
     'text!html/RoleManagement.html',
     'admin/data/DataSources',
@@ -9,147 +10,152 @@ define([
     'kendo/kendo.tabstrip.min'
 
 ], function (view) {
-    var RoleManagementView = Backbone.View.extend({
-        render: function () {
-            $('#container').append(view);
-            this.initial();
-        },
 
-        selectedGroup: null,
-        initial: function () {
-            $("#gridRoles").kendoGrid({
-                dataSource: RoleDataSource.get(),
-                sortable: true,
-                toolbar: [
-                    {
-                        name: "create",
-                        text: "Yeni Rol"
-                    }
-                ],
-                columns: [
-                    {
-                        field: "name",
-                        title: "Ad"
-                    },
-                    {
-                        field: "code",
-                        title: "Kod"
-                    },
-                    {
-                        command: [
-                            {
-                                name: "edit",
-                                text: {
-                                    edit: "",
-                                    update: "Güncelle",
-                                    cancel: "İptal"
-                                },
-                                className: "grid-command-iconfix"
+    RoleManagementView = new RobeView("RoleManagementView", view, "container");
+    
+    RoleManagementView.render = function () {
+        $('#container').append(view);
+        RoleManagementView.initialize();
+    };
+
+    RoleManagementView.initialize = function () {
+        var selectedGroup = null;
+
+        $("#gridRoles").kendoGrid({
+            dataSource: RoleDataSource.get(),
+            sortable: true,
+            toolbar: [
+                {
+                    name: "create",
+                    text: "Yeni Rol"
+                }
+            ],
+            columns: [
+                {
+                    field: "name",
+                    title: "Ad"
+                },
+                {
+                    field: "code",
+                    title: "Kod"
+                },
+                {
+                    command: [
+                        {
+                            name: "edit",
+                            text: {
+                                edit: "",
+                                update: "Güncelle",
+                                cancel: "İptal"
                             },
-                            {
-                                name: "destroy",
-                                text: "",
-                                className: "grid-command-iconfix"
-                            }
-                        ],
-                        title: "&nbsp;",
-                        width: "80px"
-                    }
-                ],
-                editable: {
-                    mode: "popup",
-                    window: {
-                        title: "Kayıt"
-                    },
-                    confirmation: "Silmek istediğinizden emin misiniz?",
-                    confirmDelete: "Yes"
-                }
-            });
-
-            $("#listAllRoles").kendoListView({
-                dataSource: RoleDataSource.get(),
-                template: "<div class='tags k-block'>#:name#</div>",
-                selectable: "single",
-                change: onListChange
-            });
-            $("#listGroupedRoles").kendoListView({
-                dataSource: GroupedRoleDataSource.get(),
-                template: "<div class='tags move  k-block'>#:name#</div><a href='javascript:' class='tagitemcls' onclick=\"removeItem(this,'#:uid#')\"><span class='k-icon k-i-close'></span></a>",
-            });
-            $("#listUnGroupedRoles").kendoListView({
-                dataSource: UnGroupedRoleDataSource.get(),
-                template: "<div class='tags move k-block'>#:name#</div>"
-            });
-            $("#listUnGroupedRoles").kendoDraggable({
-                filter: ".move",
-                hint: function (element) {
-                    return element.clone();
-                }
-            });
-            $("#listGroupedRoles").kendoDropTarget({
-                dragenter: function (e) {
-                    e.draggable.hint.css("opacity", 0.6);
-                },
-                dragleave: function (e) {
-                    e.draggable.hint.css("opacity", 1);
-                },
-                drop: function (e) {
-
-                    var data = RoleDataSource.get().view();
-                    var groupOid = selectedGroup;
-
-                    var item = UnGroupedRoleDataSource.get().getByUid(e.draggable.hint.data().uid);
-                    if (groupOid === item.oid) {
-                        return;
-                    }
-                    else {
-                        $.ajax({
-                            type: "PUT",
-                            url: AdminApp.getBackendURL() + "role/group/" + groupOid + "/" + item.oid,
-                            dataType: "json",
-                            contentType: "application/json; charset=utf-8",
-                            success: function () {
-                                GroupedRoleDataSource.get().add(item);
-                                UnGroupedRoleDataSource.get().remove(item);
-                            }
-                        });
-                    }
-                }
-            });
-            $("#btnRoleManagementHelp").kendoButton({
-                click: this.onShowHelp
-            });
-
-
-            function onListChange(e) {
-                var data = RoleDataSource.get().view(), selected = $.map(this.select(), function (item) {
-                    selectedGroup = data[$(item).index()].oid;
-                    return selectedGroup;
-                });
-
-                $.ajax({
-                    type: "GET",
-                    url: AdminApp.getBackendURL() + "role/" + selected,
-                    dataType: "json",
-                    contentType: "application/json; charset=utf-8",
-                    success: function (response) {
-                        GroupedRoleDataSource.get().data(response.roles);
-                        var oids = [];
-                        for (var i = 0; i < response.roles.length; i++) {
-                            oids.push(response.roles[i].oid);
+                            className: "grid-command-iconfix"
+                        },
+                        {
+                            name: "destroy",
+                            text: "",
+                            className: "grid-command-iconfix"
                         }
-                        oids.push(selectedGroup);
-                        var unGrouped = RoleDataSource.get().data().filter(function (elem) {
-                            return oids.indexOf(elem.oid) === -1;
-                        });
-                        UnGroupedRoleDataSource.get().data(unGrouped);
-                    }
-                });
-            };
+                    ],
+                    title: "&nbsp;",
+                    width: "80px"
+                }
+            ],
+            editable: {
+                mode: "popup",
+                window: {
+                    title: "Kayıt"
+                },
+                confirmation: "Silmek istediğinizden emin misiniz?",
+                confirmDelete: "Yes"
+            }
+        });
 
-            $("#tabstrip").kendoTabStrip();
-        },
-        onShowHelp: function () {
+        $("#listAllRoles").kendoListView({
+            dataSource: RoleDataSource.get(),
+            template: "<div class='tags k-block'>#:name#</div>",
+            selectable: "single",
+            change: onListChange
+        });
+
+        $("#listGroupedRoles").kendoListView({
+            dataSource: GroupedRoleDataSource.get(),
+            template: "<div class='tags move  k-block'>#:name#</div><a href='javascript:' class='tagitemcls' onclick=\"removeItem(this,'#:uid#')\"><span class='k-icon k-i-close'></span></a>",
+        });
+
+        $("#listUnGroupedRoles").kendoListView({
+            dataSource: UnGroupedRoleDataSource.get(),
+            template: "<div class='tags move k-block'>#:name#</div>"
+        });
+
+        $("#listUnGroupedRoles").kendoDraggable({
+            filter: ".move",
+            hint: function (element) {
+                return element.clone();
+            }
+        });
+
+        $("#listGroupedRoles").kendoDropTarget({
+            dragenter: function (e) {
+                e.draggable.hint.css("opacity", 0.6);
+            },
+            dragleave: function (e) {
+                e.draggable.hint.css("opacity", 1);
+            },
+            drop: function (e) {
+
+                var data = RoleDataSource.get().view();
+                var groupOid = selectedGroup;
+
+                var item = UnGroupedRoleDataSource.get().getByUid(e.draggable.hint.data().uid);
+                if (groupOid === item.oid) {
+                    return;
+                }
+                else {
+                    $.ajax({
+                        type: "PUT",
+                        url: AdminApp.getBackendURL() + "role/group/" + groupOid + "/" + item.oid,
+                        dataType: "json",
+                        contentType: "application/json; charset=utf-8",
+                        success: function () {
+                            GroupedRoleDataSource.get().add(item);
+                            UnGroupedRoleDataSource.get().remove(item);
+                        }
+                    });
+                }
+            }
+        });
+
+        $("#btnRoleManagementHelp").kendoButton({
+            click: this.onShowHelp
+        });
+
+        function onListChange(e) {
+            var data = RoleDataSource.get().view(), selected = $.map(this.select(), function (item) {
+                selectedGroup = data[$(item).index()].oid;
+                return selectedGroup;
+            });
+
+            $.ajax({
+                type: "GET",
+                url: AdminApp.getBackendURL() + "role/" + selected,
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                success: function (response) {
+                    GroupedRoleDataSource.get().data(response.roles);
+                    var oids = [];
+                    for (var i = 0; i < response.roles.length; i++) {
+                        oids.push(response.roles[i].oid);
+                    }
+                    oids.push(selectedGroup);
+                    var unGrouped = RoleDataSource.get().data().filter(function (elem) {
+                        return oids.indexOf(elem.oid) === -1;
+                    });
+                    UnGroupedRoleDataSource.get().data(unGrouped);
+                }
+            });
+        };
+
+        function onShowHelp() {
             wnd = $("#roleManagementHelpWindow").kendoWindow({
                 title: "Yardım",
                 modal: true,
@@ -159,8 +165,9 @@ define([
             }).data("kendoWindow");
             wnd.center().open();
 
-        },
-        removeItem: function (e, id) {
+        };
+
+        function removeItem(e, id) {
             var item = GroupedRoleDataSource.get().getByUid(id);
 
             $.ajax({
@@ -181,7 +188,10 @@ define([
                 }
             });
         }
-    });
+
+        $("#tabstrip").kendoTabStrip();
+    };
+
     return RoleManagementView;
 });
 
