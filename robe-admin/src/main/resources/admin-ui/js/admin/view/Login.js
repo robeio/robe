4ed40a8a6644e3963ac94679d0d977ade9099162
kaan.alkim <kaan.alkim@mebitech.com>
2014-03-17 15:04:16 +0200

    #ROBE-79 modified according to RobeView

--- robe-admin/src/main/resources/admin-ui/js/admin/view/Login.js
+++ robe-admin/src/main/resources/admin-ui/js/admin/view/Login.js
@@ -1,4 +1,5 @@
 //@ sourceURL=Login.js
+var LoginView;
 define([
     'text!html/Login.html',
 
@@ -6,64 +7,62 @@ define([
     'zebra_cookie',
     'cryptojs/core-min',
     'cryptojs/enc-base64-min',
-    'cryptojs/sha256'
-
+    'cryptojs/sha256',
+    'robe/view/Page'
 ], function (view) {
-    var LoginView = Backbone.View.extend({
-        render: function () {
-            $('#dialogMessage').append(view);
-            this.initial();
-        },
-        initial: function () {
-            var token = $.cookie.read("auth-token");
-            $("#username").val("admin@robe.io");
-            $("#password").val("123123");
 
+    LoginView = new RobeView("LoginView", view, "container");
+    LoginView.render = function () {
+        $('#dialogMessage').append(view);
+        LoginView.initialize();
+    };
 
-            $('#loginError').hide();
-            var me = this;
-            $('#login-button').kendoButton({
-                click: function (token) {
-                    $.ajax({
-                        type: "POST",
-                        url: AdminApp.getBackendURL() + "authentication/login",
-                        data: JSON.stringify({username: $("#username").val(), password: CryptoJS.SHA256($("#password").val()).toString()}),
-                        contentType: "application/json; charset=utf-8",
-                        success: function (response) {
-                            $.cookie.write("userEmail", $("#username").val());
-                            $(document.body).unbind("keydown");
-                            $('#dialog').data("kendoWindow").close();
-                            me.parentPage.loadMenu();
+    LoginView.initialize = function () {
+        var token = $.cookie.read("auth-token");
+        $("#username").val("admin@robe.io");
+        $("#password").val("123123");
+
+        $('#loginError').hide();
+        var me = this;
+        $('#login-button').kendoButton({
+            click: function (token) {
+                $.ajax({
+                    type: "POST",
+                    url: AdminApp.getBackendURL() + "authentication/login",
+                    data: JSON.stringify({username: $("#username").val(), password: CryptoJS.SHA256($("#password").val()).toString()}),
+                    contentType: "application/json; charset=utf-8",
+                    success: function (response) {
+                        $.cookie.write("userEmail", $("#username").val());
+                        $(document.body).unbind("keydown");
+                        $('#dialog').data("kendoWindow").close();
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        console.log(errorThrown);
+                    },
+                    statusCode: {
+                        401: function () {
+                            $('#loginError').show();
                         },
-                        error: function (jqXHR, textStatus, errorThrown) {
-                            console.log(errorThrown);
-                        },
-                        statusCode: {
-                            401: function () {
-                                $('#loginError').show();
-                            },
-                            422: function (xhr) {
-                                var errors = JSON.parse(xhr.responseText);
-                                var msg = "";
-                                $.each(errors, function (index, error) {
-                                    msg += "<br> <b>" + error.name + ":</b> " + error.message;
-                                });
-                                robe.App.instance.showDialog(msg);
-                            }
+                        422: function (xhr) {
+                            var errors = JSON.parse(xhr.responseText);
+                            var msg = "";
+                            $.each(errors, function (index, error) {
+                                msg += "<br> <b>" + error.name + ":</b> " + error.message;
+                            });
+                            robe.App.instance.showDialog(msg);
                         }
-                    });
-                },
-                imageUrl: "./icon/checkmark.png"
-            });
-            $(document.body).keydown(function (e) {
-                if (e.keyCode == 13) {
-                    $("#login-button")[0].focus();
-                }
-            });
-        },
-        parentPage: null
-    });
-    // Our module now returns our view
+                    }
+                });
+            },
+            imageUrl: "./icon/checkmark.png"
+        });
+        $(document.body).keydown(function (e) {
+            if (e.keyCode == 13) {
+                $("#login-button")[0].focus();
+            }
+        });
+    };
+
     return LoginView;
 });
 
--- robe-admin/src/main/resources/admin-ui/js/admin/view/Workspace.js
+++ robe-admin/src/main/resources/admin-ui/js/admin/view/Workspace.js
@@ -1,4 +1,5 @@
 //@ sourceURL=Workspace.js
+var WorkspaceView;
 define([
     'text!html/Workspace.html',
     'view/Login',
@@ -11,149 +12,68 @@ define([
     'kendo/kendo.progressbar.min',
     'kendo/kendo.button.min',
     'kendo/kendo.window.min',
-    'kendo/kendo.panelbar.min'
+    'kendo/kendo.panelbar.min',
+    'robe/view/Page'
 ], function (view, LoginView, ProfileManagementView) {
-    var WorkspaceView = Backbone.View.extend({
-        render: function () {
-            kendo.destroy($('#body'));
-            $('#body').html('');
-            $('#body').append(view);
-            this.initial();
-        },
-        initial: function () {
-            var me = this;
-            $("#progressBar").kendoProgressBar({
-                min: 0,
-                max: 1,
-                type: "value",
-                showStatus: false,
-                animation: {
-                    duration: 200
-                }
-            });
-            kendo.destroy($("#container"));
-            $("#container").html("");
-            $("#profile").click(function () {
 
-                showProfileDialog(null, "Profil Bilgileri");
-                kendo.destroy($('#dialogMessage'));
-                $('#dialogMessage').html('');
-                var profileView = new ProfileManagementView();
-                profileView.parentPage = this;
-                profileView.render();
-            });
+    WorkspaceView = new RobeView("WorkspaceView", view, "container");
+    WorkspaceView.render = function () {
+        kendo.destroy($('#body'));
+        $('#body').html('');
+        $('#body').append(view);
+        WorkspaceView.initialize();
+    };
 
-            $("#logout").click(function () {
-                $.cookie.destroy("auth-token");
-                location.reload();
-            });
-
-            $("#settings").kendoButton({
-                click: this.onClickSettingsButton
-            });
-
-
-            $('#dialog').kendoWindow({
-                actions: ["Close"],
-                modal: true,
-                visible: false
-            });
-
-
-            $(document).ajaxStart(function () {
-                me.showIndicator(true);
-            });
-            $(document).ajaxStop(function () {
-                me.showIndicator(false);
-            });
-
-            this.loadLogin();
-        },
-
-        onClickSettingsButton: function (e) {
-            $("#dropdownMenu").toggle("slow");
-        },
-        loadMenu: function () {
-            var me = this;
-            $.ajax({
-                type: "GET",
-                url: AdminApp.getBackendURL() + "menu/user",
-                dataType: "json",
-                contentType: "application/json; charset=utf-8",
-                success: function (response) {
-                    me.addIcons(response[0]);
-                    $('#menu').kendoPanelBar({
-                        dataSource: response[0].items,
-                        select: function (e) {
-                            var selection = "k-";
-                            for (var i = 0; i < e.item.classList.length; i++) {
-                                var css = e.item.classList[i];
-                                if (css.indexOf("command:") == 0) {
-                                    selection = css.substring(8);
-                                    break;
-                                }
-                            }
-                            me.openMenuItem(selection);
-                        }
-                    });
-
-                }
-            });
-        },
-        addIcons: function (menu) {
-            if (menu.hasOwnProperty("items")) {
-                for (var i = 0; i < menu.items.length; i++) {
-                    this.addIcons(menu.items[i]);
-                }
+    WorkspaceView.initialize = function () {
+        var me = this;
+        $("#progressBar").kendoProgressBar({
+            min: 0,
+            max: 1,
+            type: "value",
+            showStatus: false,
+            animation: {
+                duration: 200
             }
-            menu.imageUrl = "./icon/menu/" + menu.cssClass.substring(8) + ".png";
-        },
+        });
+        kendo.destroy($("#container"));
+        $("#container").html("");
+        $("#profile").click(function () {
 
-        previousItem: "",
-        openMenuItem: function (menuitem) {
-
-            if (menuitem.indexOf("k-") == 0)
-                return;
-            if (this.previousItem == menuitem)
-                return;
-            else
-                this.previousItem = menuitem;
-            try {
-                kendo.destroy($('#container'));
-                $('#container').html('');
-                window.location.href = '#/' + menuitem;
-            } catch (e) {
-                console.error(menuitem + " JS: " + e);
-            }
-            kendo.fx($("#container")).fade("in").play();
-        },
-
-        showIndicator: function (show) {
-            if (show)
-                $("#progressBar").data("kendoProgressBar").value(0);
-            else
-                $("#progressBar").data("kendoProgressBar").value(1);
-        },
-
-        showDialog: function (message, title) {
-            if (message != null)
-                $('#dialogMessage').html(message);
-            if (title == null)
-                title = "";
-            $('#dialog').data("kendoWindow").title(title);
-            $('#dialog').data("kendoWindow").center();
-            $('#dialog').data("kendoWindow").open();
-        },
-
-        loadLogin: function () {
-            this.showDialog(null, "Giriş");
+            showProfileDialog(null, "Profil Bilgileri");
             kendo.destroy($('#dialogMessage'));
             $('#dialogMessage').html('');
-            var loginView = new LoginView();
-            loginView.parentPage = this;
-            loginView.render();
-        }
-    });
+            var profileView = new ProfileManagementView();
+            profileView.parentPage = this;
+            profileView.render();
+        });
+
+        $("#logout").click(function () {
+            $.cookie.destroy("auth-token");
+            location.reload();
+        });
+
+        $("#settings").kendoButton({
+            click: onClickSettingsButton
+        });
+
+
+        $('#dialog').kendoWindow({
+            actions: ["Close"],
+            modal: true,
+            visible: false
+        });
+
+
+        $(document).ajaxStart(function () {
+            showIndicator(true);
+        });
+        $(document).ajaxStop(function () {
+            showIndicator(false);
+        });
+
+        loadMenu();
+        loadLogin();
+    };
 
     function showProfileDialog(message, title) {
         if (message != null)
@@ -164,6 +84,91 @@ define([
         $('#dialog').data("kendoWindow").center();
         $('#dialog').data("kendoWindow").open();
     }
+
+    function onClickSettingsButton(e) {
+        $("#dropdownMenu").toggle("slow");
+    };
+
+    function loadMenu() {
+        var me = this;
+        $.ajax({
+            type: "GET",
+            url: AdminApp.getBackendURL() + "menu/user",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function (response) {
+                addIcons(response[0]);
+                $('#menu').kendoPanelBar({
+                    dataSource: response[0].items,
+                    select: function (e) {
+                        var selection = "k-";
+                        for (var i = 0; i < e.item.classList.length; i++) {
+                            var css = e.item.classList[i];
+                            if (css.indexOf("command:") == 0) {
+                                selection = css.substring(8);
+                                break;
+                            }
+                        }
+                        openMenuItem(selection);
+                    }
+                });
+
+            }
+        });
+    };
+
+    function addIcons(menu) {
+        if (menu.hasOwnProperty("items")) {
+            for (var i = 0; i < menu.items.length; i++) {
+                addIcons(menu.items[i]);
+            }
+        }
+        menu.imageUrl = "./icon/menu/" + menu.cssClass.substring(8) + ".png";
+    };
+
+    var previousItem = "";
+
+    function openMenuItem(menuitem) {
+
+        if (menuitem.indexOf("k-") == 0)
+            return;
+        if (this.previousItem == menuitem)
+            return;
+        else
+            this.previousItem = menuitem;
+        try {
+            kendo.destroy($('#container'));
+            $('#container').html('');
+            window.location.href = '#/' + menuitem;
+        } catch (e) {
+            console.error(menuitem + " JS: " + e);
+        }
+        kendo.fx($("#container")).fade("in").play();
+    };
+
+    function showIndicator(show) {
+        if (show)
+            $("#progressBar").data("kendoProgressBar").value(0);
+        else
+            $("#progressBar").data("kendoProgressBar").value(1);
+    };
+
+    function showDialog(message, title) {
+        if (message != null)
+            $('#dialogMessage').html(message);
+        if (title == null)
+            title = "";
+        $('#dialog').data("kendoWindow").title(title);
+        $('#dialog').data("kendoWindow").center();
+        $('#dialog').data("kendoWindow").open();
+    };
+
+    function loadLogin() {
+        showDialog(null, "Giriş");
+        kendo.destroy($('#dialogMessage'));
+        $('#dialogMessage').html('');
+        LoginView.render();
+    };
 
     // Our module now returns our view
     return WorkspaceView;
