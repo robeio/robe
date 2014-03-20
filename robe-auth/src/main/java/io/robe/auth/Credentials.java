5d302a1feb177bb1e6e9089f660b8eac03ca36fb
admin <kerem.avci@mebitech.com>
2014-03-20 10:28:53 +0200

    ROBE-1

---
+++ robe-auth/src/main/java/io/robe/auth/Credentials.java
@@ -0,0 +1,15 @@
+package io.robe.auth;
+
+import java.util.Set;
+
+
+/**
+ * Basic Credential model for Auth operations. Secure and lightweight injectable model.
+ */
+public interface Credentials  {
+
+    public Set<String> getPermissions();
+
+    String getUsername();
+}
+
