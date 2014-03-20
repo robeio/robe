c92971ef23571fbb26696019c102a90f38ced5ff
Seray Uzgur <seray.uzgur@mebitech.com>
2014-03-20 16:26:14 +0200

    ROBE-82 sonar

--- robe-auth/src/main/java/io/robe/auth/TokenWrapper.java
+++ robe-auth/src/main/java/io/robe/auth/TokenWrapper.java
@@ -8,10 +8,10 @@ public class TokenWrapper<T extends IsToken> {
 
     private static Class<? extends IsToken> type;
 
-    private TokenWrapper(){
+    private TokenWrapper() {
     }
 
-    public static <I extends  IsToken> void  initialize(Class<I> clazz) {
+    public static <I extends IsToken> void initialize(Class<I> clazz) {
         type = clazz;
     }
 
@@ -22,25 +22,18 @@ public class TokenWrapper<T extends IsToken> {
     /**
      * Creates an access token with the given {@link io.robe.auth.Credentials}
      *
-     * @param username Username to create token
+     * @param username   Username to create token
      * @param attributes Extra attributes to include in token. (role,lang,...)
-
      * @return {@link io.robe.auth.IsToken} instance with n second expiration.
      * @throws java.lang.IllegalAccessException,java.lang.InstantiationException
      */
-    public static IsToken createToken(String username, Map<String, String> attributes) throws Exception {
-        try {
+    public static IsToken createToken(String username, Map<String, String> attributes) throws IllegalAccessException, InstantiationException {
         IsToken token = type.newInstance();
-
-            token.setUserAccountName(username);
+        token.setUserAccountName(username);
         //TODO: get expiration from properties.
         token.setExpiration(600);
-        if(attributes != null && attributes.size()>0)
+        if (attributes != null && attributes.size() > 0)
             token.addAttributes(attributes);
         return token;
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return null;
     }
 }
