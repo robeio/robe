871cabf76b1bc1c44e888a2c7e66f5363fc40ec1
Seray Uzgur <seray.uzgur@mebitech.com>
2014-03-28 12:17:43 +0200

    ROBE-90 parser Enum and test

---
+++ robe-convert/src/test/java/io/robe/convert/excel/ParsersTest.java
@@ -0,0 +1,37 @@
+package io.robe.convert.excel;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+
+public class ParsersTest {
+    @Test
+    public void testSwitch() throws Exception {
+
+        String clazz = BigDecimal.class.getSimpleName().toUpperCase(Locale.ENGLISH);
+        System.out.println(Parsers.valueOf(clazz).getParser().getClass().getName());
+
+        clazz = Boolean.class.getSimpleName().toUpperCase(Locale.ENGLISH);
+        System.out.println(Parsers.valueOf(clazz).getParser().getClass().getName());
+
+        clazz = Byte.class.getSimpleName().toUpperCase(Locale.ENGLISH);
+        System.out.println(Parsers.valueOf(clazz).getParser().getClass().getName());
+
+        clazz = Double.class.getSimpleName().toUpperCase(Locale.ENGLISH);
+        System.out.println(Parsers.valueOf(clazz).getParser().getClass().getName());
+
+        clazz = int.class.getSimpleName().toUpperCase(Locale.ENGLISH);
+        System.out.println(Parsers.valueOf(clazz).getParser().getClass().getName());
+      
+        clazz = Integer.class.getSimpleName().toUpperCase(Locale.ENGLISH);
+        System.out.println(Parsers.valueOf(clazz).getParser().getClass().getName());
+
+        clazz = Long.class.getSimpleName().toUpperCase(Locale.ENGLISH);
+        System.out.println(Parsers.valueOf(clazz).getParser().getClass().getName());
+
+        clazz = String.class.getSimpleName().toUpperCase(Locale.ENGLISH);
+        System.out.println(Parsers.valueOf(clazz.toUpperCase(Locale.ENGLISH)).getParser().getClass().getName());
+        
+    }
+}
