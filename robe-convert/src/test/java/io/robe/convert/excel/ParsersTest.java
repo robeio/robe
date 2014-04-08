32cc1465545a54890b76fff311c697d1ae882a99
Seray Uzgur <seray.uzgur@mebitech.com>
2014-04-08 17:38:17 +0300

    ROBE-108 iterative XLS,XLXS importer

--- robe-convert/src/test/java/io/robe/convert/excel/ParsersTest.java
+++ robe-convert/src/test/java/io/robe/convert/excel/ParsersTest.java
@@ -1,5 +1,6 @@
 package io.robe.convert.excel;
 
+import io.robe.convert.excel.parsers.Parsers;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -23,7 +24,7 @@ public class ParsersTest {
 
         clazz = int.class.getSimpleName().toUpperCase(Locale.ENGLISH);
         System.out.println(Parsers.valueOf(clazz).getParser().getClass().getName());
-      
+
         clazz = Integer.class.getSimpleName().toUpperCase(Locale.ENGLISH);
         System.out.println(Parsers.valueOf(clazz).getParser().getClass().getName());
 
@@ -32,6 +33,6 @@ public class ParsersTest {
 
         clazz = String.class.getSimpleName().toUpperCase(Locale.ENGLISH);
         System.out.println(Parsers.valueOf(clazz.toUpperCase(Locale.ENGLISH)).getParser().getClass().getName());
-        
+
     }
 }
