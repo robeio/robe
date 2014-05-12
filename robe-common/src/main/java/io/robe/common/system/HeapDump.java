235b08ccba57cfbc5488b271605c8aee30351f31
hasanmumin <hasan.mumin@acedemand.com>
2014-05-12 14:13:18 +0300

    ROBE-121

--- robe-common/src/main/java/io/robe/common/system/HeapDump.java
+++ robe-common/src/main/java/io/robe/common/system/HeapDump.java
@@ -54,8 +54,7 @@ public class HeapDump {
     private static HotSpotDiagnosticMXBean getHotspotMBean() {
         try {
             MBeanServer server = ManagementFactory.getPlatformMBeanServer();
-            HotSpotDiagnosticMXBean bean = ManagementFactory.newPlatformMXBeanProxy(server, HOTSPOT_BEAN_NAME, HotSpotDiagnosticMXBean.class);
-            return bean;
+            return  ManagementFactory.newPlatformMXBeanProxy(server, HOTSPOT_BEAN_NAME, HotSpotDiagnosticMXBean.class);
         } catch (RuntimeException re) {
             throw re;
         } catch (Exception exp) {
