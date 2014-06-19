b54367767cb3b723823d3b4c126e4ddb78dc6fd8
Seray Uzgur <seray.uzgur@mebitech.com>
2014-06-19 18:11:09 +0300

    ROBE-148 Validator bug fixed. All validation keys are unique now

--- robe-admin/src/main/resources/admin-ui/js/robe/Validations.js
+++ robe-admin/src/main/resources/admin-ui/js/robe/Validations.js
@@ -10,29 +10,33 @@ function getValidations(field, label, isRequired, isEmail, minlength, maxlength,
             message: label + " yazımı hatalı."
         };
     if (minlength)
-        validations["minlength"] = function (input) {
+        validations[field + "_minlength"] = function (input) {
             if ((input.is("[data-bind='value:" + field + "']") && input.val().length < minlength)) {
-                input.attr("data-minlength-msg", label + " alanı en az " + minlength + " karakter olmalı.");
+                input.attr("data-" + field + "_minlength-msg", label + " alanı en az " + minlength + " karakter olmalı.");
                 return false;
             }
             return true;
 
         };
-    if (maxlength)
-        validations["maxlength"] = function (input) {
-            if ((input.is("[data-bind='value:" + field + "']") && input.val().length > maxlength)) {
-                input.attr("data-maxlength-msg", label + " alanı en fazla " + maxlength + " karakter olmalı.");
+    if (maxlength) {
+        validations[field + "_maxlength"] = function (input) {
+            var isInputField = input.is('[data-bind=\"value:' + field + '\"]');
+            if (isInputField && input.val().length > maxlength) {
+                input.attr('data-' + field + '_maxlength-msg', label + ' alanı en fazla ' + maxlength + ' karakter olmalı.');
                 return false;
             }
-            return true;
-        };
+            return true
+        }
+    }
+
     if (regexp)
-        validations["regexp"] = function (input) {
+        validations[field + "_regexp"] = function (input) {
             if ((input.is("[data-bind='value:" + field + "']") && !(new RegExp(regexp).test(input.val())))) {
-                input.attr("data-regexp-msg", label + " alanı " + regexp + " formatına uymalı.");
+                input.attr("data-" + field + "_regexp-msg", label + " alanı " + regexp + " formatına uymalı.");
                 return false;
             }
             return true;
         };
+    console.log(validations);
     return validations;
 }
