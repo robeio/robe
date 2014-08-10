2539fee76cbfb82ef05e15b2335dacefc7638261
hasan.mumin <hasanmumin>
2014-08-10 19:55:07 +0300

    ROBE-143 fixed

--- robe-convert/src/test/java/io/robe/convert/json/JSONExporterTest.java
+++ robe-convert/src/test/java/io/robe/convert/json/JSONExporterTest.java
@@ -4,16 +4,23 @@ import io.robe.convert.SamplePojo;
 import io.robe.convert.TestData;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.OutputStream;
 
 public class JSONExporterTest {
+
+    private String SAMPLE = JSONExporterTest.class.getClassLoader().getResource("sample.json").getFile();
+
+
     @Test
     public void testExportStream() throws Exception {
         JSONExporter exporter = new JSONExporter();
 
-
-        OutputStream outputStream = System.out;
+        OutputStream outputStream = new FileOutputStream(new File(SAMPLE));
 
         exporter.exportStream(SamplePojo.class, outputStream, TestData.getData().iterator());
+
+        outputStream.flush();
     }
 }
