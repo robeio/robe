719be212e20395cbfb5e5862170f2a7221e3ddb4
hasan.mumin <hasan.mumin@mebitech.com>
2014-11-05 16:03:53 +0200

    #ROBE-199 isEmptyOrNull Validations

--- robe-common/src/main/java/io/robe/common/utils/Validations.java
+++ robe-common/src/main/java/io/robe/common/utils/Validations.java
@@ -2,7 +2,6 @@ package io.robe.common.utils;
 
 public class Validations {
 
-
     public static boolean isEmptyOrNull(String pattern) {
         return pattern == null || pattern.trim().equals("");
     }
