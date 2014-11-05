12595069be6d7746ec58b8cf665af484d2d1cd13
hasan.mumin <hasan.mumin@mebitech.com>
2014-11-05 16:00:57 +0200

    ROBE-199

--- robe-common/src/main/java/io/robe/common/utils/Validations.java
+++ robe-common/src/main/java/io/robe/common/utils/Validations.java
@@ -1,9 +1,8 @@
 package io.robe.common.utils;
 
 public class Validations {
+
     public static boolean isEmptyOrNull(String pattern) {
         return pattern == null || pattern.trim().equals("");
     }
-
-
 }
