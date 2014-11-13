16b9953c0be5bb26fb44f06991e27b132af276f1
hasan.mumin <hasan.mumin@mebitech.com>
2014-11-13 11:49:39 +0200

    ROBE-209

--- robe-admin/src/main/java/io/robe/admin/RobeApplication.java
+++ robe-admin/src/main/java/io/robe/admin/RobeApplication.java
@@ -32,7 +32,6 @@ import java.util.List;
 public class RobeApplication<T extends RobeServiceConfiguration> extends Application<T> {
 
 
-<<<<<<< HEAD
     private HibernateBundle<T> hibernateBundle = null;
 
     public static void main(String[] args) throws Exception {
@@ -105,98 +104,8 @@ public class RobeApplication<T extends RobeServiceConfiguration> extends Applica
     }
 
     private void addExceptionMappers(Environment environment) {
-//        ResourceConfig jrConfig = environment.jersey().getResourceConfig();
-//        Set<Object> dwSingletons = jrConfig.getSingletons();
-//        List<Object> singletonsToRemove = new ArrayList<Object>();
-//        for (Object s : dwSingletons) {
-//            if (s instanceof ExceptionMapper && s.getClass().getName().startsWith("io.dropwizard.jersey.InvalidEntityExceptionMapper")) {
-//                singletonsToRemove.add(s);
-//            }
-//        }
-//
-//        for (Object s : singletonsToRemove) {
-//            jrConfig.getSingletons().remove(s);
-//        }
         environment.jersey().register(new RobeExceptionMapper());
 
     }
-=======
-	private HibernateBundle<T> hibernateBundle = null;
-
-	public static void main(String[] args) throws Exception {
-		new RobeApplication().run(args);
-	}
-
-	public HibernateBundle getHibernateBundle() {
-		return hibernateBundle;
-	}
-
-	/**
-	 * Adds
-	 * Hibernate bundle for PROVIDER connection
-	 * Asset bundle for io.robe.admin screens and
-	 * Class scanners for
-	 * <ul>
-	 * <li>Entities</li>
-	 * <li>HealthChecks</li>
-	 * <li>Providers</li>
-	 * <li>InjectableProviders</li>
-	 * <li>Resources</li>
-	 * <li>Tasks</li>
-	 * <li>Managed objects</li>
-	 * </ul>
-	 *
-	 * @param bootstrap
-	 */
-	@Override
-	public void initialize(Bootstrap<T> bootstrap) {
-		hibernateBundle = new HibernateBundle<T>();
-		QuartzBundle<T> quartzBundle = new QuartzBundle<T>();
-		MailBundle<T> mailBundle = new MailBundle<T>();
-		TokenBasedAuthBundle<T> authBundle = new TokenBasedAuthBundle<T>();
-
-		bootstrap.addBundle(hibernateBundle);
-		bootstrap.addBundle(authBundle);
-		bootstrap.addBundle(quartzBundle);
-		bootstrap.addBundle(new ViewBundle());
-		bootstrap.addBundle(mailBundle);
-		bootstrap.addBundle(new ConfiguredAssetBundle<T>());
-
-		List<Module> modules = new LinkedList<Module>();
-		modules.add(new HibernateModule(hibernateBundle));
-		modules.add(new AuthenticatorModule(authBundle, bootstrap.getMetricRegistry()));
-		modules.add(new QuartzModule(quartzBundle));
-		modules.add(new MailModule(mailBundle));
-
-		bootstrap.addBundle(new GuiceBundle<T>(modules, bootstrap.getApplication().getConfigurationClass()));
-		bootstrap.addCommand(new InitializeCommand(this, hibernateBundle));
-
-
-		//TODO: Bad way to get it. Will change it later.
-		ByHibernate.setHibernateBundle(hibernateBundle);
-
-	}
-
-
-	/**
-	 * {@inheritDoc}
-	 * In addition adds exception mapper.
-	 *
-	 * @param configuration
-	 * @param environment
-	 * @throws Exception
-	 */
-	@UnitOfWork
-	@Override
-	public void run(T configuration, Environment environment) throws Exception {
-		addExceptionMappers(environment);
-		RobeConfigurationManager.getInstance().setGuiceConfiguration(configuration.getGuiceConfiguration());
-	}
-
-	private void addExceptionMappers(Environment environment) {
-		environment.jersey().register(new RobeExceptionMapper());
-
-	}
->>>>>>> 4df597b9b796dbae11e71768b12dad135a55e899
 
 }
