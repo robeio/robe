5833b879b1bf52a71fd1549d9333853832f5cc01
Seray Uzgur <seray.uzgur@mebitech.com>
2014-11-20 15:58:26 +0200

    ROBE-218 refactoring and xls & xlsx exporter test fix

--- robe-convert/src/test/java/io/robe/convert/excel/XLSExporterTest.java
+++ robe-convert/src/test/java/io/robe/convert/excel/XLSExporterTest.java
@@ -5,11 +5,14 @@ import io.robe.convert.FileUtil;
 import io.robe.convert.SamplePojo;
 import io.robe.convert.TestData;
 import io.robe.convert.excel.exporter.XLSExporter;
+import io.robe.convert.excel.importer.XLSImporter;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.List;
 
 public class XLSExporterTest {
 
@@ -17,25 +20,47 @@ public class XLSExporterTest {
     @Test
     public void exportStream() throws Exception {
 
+        // Write to temp file
         File outputFile = FileUtil.getRandomTempFile();
         OutputStream outputStream = new FileOutputStream(outputFile);
         XLSExporter<SamplePojo> xlsExporter = new XLSExporter(SamplePojo.class, false);
         xlsExporter.exportStream(outputStream, TestData.getData().iterator());
         outputStream.flush();
         outputStream.close();
-        System.out.println(outputFile.getPath());
 
-        // And with title
-        File outputFile2 = FileUtil.getRandomTempFile();
-        outputStream = new FileOutputStream(outputFile2);
-        XLSExporter<SamplePojo> xlsExporter2 = new XLSExporter(SamplePojo.class, true);
-        xlsExporter2.exportStream(outputStream, TestData.getData().iterator());
+        XLSImporter<SamplePojo> xlsImporter = new XLSImporter(SamplePojo.class, false);
+        List<SamplePojo> samplePojos = xlsImporter.importStream(new FileInputStream(outputFile.getPath()));
+        assert samplePojos.size() == TestData.getData().size();
+        int index = 0;
+
+        for (SamplePojo importedPojo : samplePojos) {
+            SamplePojo ref = TestData.getData().get(index++);
+            assert importedPojo.equals(ref);
+            System.out.println(ref);
+        }
+
+    }
+
+    @Test
+    public void exportStreamWithTitle() throws Exception {
+        // Write to temp file
+        File outputFile = FileUtil.getRandomTempFile();
+        OutputStream outputStream = new FileOutputStream(outputFile);
+        XLSExporter<SamplePojo> xlsExporter = new XLSExporter(SamplePojo.class, true);
+        xlsExporter.exportStream(outputStream, TestData.getData().iterator());
         outputStream.flush();
         outputStream.close();
-        System.out.println(outputFile2.getPath());
 
+        XLSImporter<SamplePojo> xlsImporter = new XLSImporter(SamplePojo.class, true);
+        List<SamplePojo> samplePojos = xlsImporter.importStream(new FileInputStream(outputFile.getPath()));
+        assert samplePojos.size() == TestData.getData().size();
+        int index = 0;
 
-        //TODO: Check with imported data
+        for (SamplePojo importedPojo : samplePojos) {
+            SamplePojo ref = TestData.getData().get(index++);
+            assert importedPojo.equals(ref);
+            System.out.println(ref);
+        }
 
     }
 }
--- robe-convert/src/test/java/io/robe/convert/excel/XLSXExporterTest.java
+++ robe-convert/src/test/java/io/robe/convert/excel/XLSXExporterTest.java
@@ -1,32 +1,64 @@
 package io.robe.convert.excel;
 
+import io.robe.convert.FileUtil;
 import io.robe.convert.SamplePojo;
 import io.robe.convert.TestData;
 import io.robe.convert.excel.exporter.XLSXExporter;
+import io.robe.convert.excel.importer.XLSXImporter;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.List;
 
 public class XLSXExporterTest {
 
-    private String SAMPLE = XLSXExporterTest.class.getClassLoader().getResource("sample.xlsx").getFile();
-    private static final Logger LOGGER = LoggerFactory.getLogger(XLSXExporterTest.class);
-
     @Test
     public void exportStream() throws Exception {
-        File exportFile = new File(SAMPLE);
-        OutputStream outputStream = new FileOutputStream(exportFile);
-        LOGGER.info(" New excel file exported to this location : " + exportFile.getCanonicalPath() +
-                " with size : " + exportFile.getTotalSpace() + " bytes");
 
+        // Write to temp file
+        File outputFile = FileUtil.getRandomTempFile();
+        OutputStream outputStream = new FileOutputStream(outputFile);
+        XLSXExporter<SamplePojo> xlsxExporter = new XLSXExporter(SamplePojo.class, false);
+        xlsxExporter.exportStream(outputStream, TestData.getData().iterator());
+        outputStream.flush();
+        outputStream.close();
+
+        XLSXImporter<SamplePojo> xlsImporter = new XLSXImporter(SamplePojo.class, false);
+        List<SamplePojo> samplePojos = xlsImporter.importStream(new FileInputStream(outputFile.getPath()));
+        assert samplePojos.size() == TestData.getData().size();
+        int index = 0;
+
+        for (SamplePojo importedPojo : samplePojos) {
+            SamplePojo ref = TestData.getData().get(index++);
+            assert importedPojo.equals(ref);
+            System.out.println(ref);
+        }
+
+    }
+
+    @Test
+    public void exportStreamWithTitle() throws Exception {
+        // Write to temp file
+        File outputFile = FileUtil.getRandomTempFile();
+        OutputStream outputStream = new FileOutputStream(outputFile);
         XLSXExporter<SamplePojo> xlsxExporter = new XLSXExporter(SamplePojo.class, true);
         xlsxExporter.exportStream(outputStream, TestData.getData().iterator());
-
         outputStream.flush();
+        outputStream.close();
+
+        XLSXImporter<SamplePojo> xlsxImporter = new XLSXImporter(SamplePojo.class, true);
+        List<SamplePojo> samplePojos = xlsxImporter.importStream(new FileInputStream(outputFile.getPath()));
+        assert samplePojos.size() == TestData.getData().size();
+        int index = 0;
+
+        for (SamplePojo importedPojo : samplePojos) {
+            SamplePojo ref = TestData.getData().get(index++);
+            assert importedPojo.equals(ref);
+            System.out.println(ref);
+        }
 
     }
 }
