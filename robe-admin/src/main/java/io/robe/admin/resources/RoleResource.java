38ea7bef78226cab90c34c0ae37cc1888a1e16af
EA <erhan.akbaba@mebitech.com>
2016-03-31 15:21:42 +0300

    RoleResource updated.

--- robe-admin/src/main/java/io/robe/admin/resources/RoleResource.java
+++ robe-admin/src/main/java/io/robe/admin/resources/RoleResource.java
@@ -1,38 +1,28 @@
 package io.robe.admin.resources;
 
-import com.google.common.base.Optional;
 import com.google.inject.Inject;
 import io.dropwizard.auth.Auth;
 import io.dropwizard.hibernate.UnitOfWork;
 import io.robe.admin.hibernate.dao.RoleDao;
-import io.robe.admin.hibernate.dao.RoleGroupDao;
 import io.robe.admin.hibernate.entity.Role;
-import io.robe.admin.hibernate.entity.RoleGroup;
 import io.robe.auth.Credentials;
-import io.robe.common.exception.RobeRuntimeException;
 import org.hibernate.FlushMode;
 
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static org.hibernate.CacheMode.GET;
-
 
 @Path("roles")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class RoleResource {
 
-    private static final String ALREADY_USED = " already used by another role. Please use different code.";
-
     @Inject
     private RoleDao roleDao;
-
-    @Inject
-    private RoleGroupDao roleGroupDao;
 
     @GET
     @UnitOfWork(readOnly = true, cacheMode = GET, flushMode = FlushMode.MANUAL)
@@ -44,79 +34,42 @@ public class RoleResource {
     @GET
     @UnitOfWork(readOnly = true, cacheMode = GET, flushMode = FlushMode.MANUAL)
     public Role get(@Auth Credentials credentials, @PathParam("id") String id) {
-
-        Role response = roleDao.findById(id);
-
-        return readRoleHierarchical(response);
-    }
-
-    private Role readRoleHierarchical(Role response) {
-
-        List<RoleGroup> roleGroups = roleGroupDao.findByGroupOId(response.getOid());
-
-        for (RoleGroup roleGroup : roleGroups) {
-            Role role = roleDao.findById(roleGroup.getRoleOid());
-            response.getRoles().add(readRoleHierarchical(role));
+        Role entity = roleDao.findById(id);
+        if (entity == null) {
+            throw new WebApplicationException(Response.status(404).build());
         }
-
-        return response;
-    }
-
-    @Path("{id}")
-    @PUT
-    @UnitOfWork
-    public Role update(@Auth Credentials credentials, @PathParam("id") String id, @Valid Role role) {
-
-        roleDao.detach(role);
-        Optional<Role> checkRole = roleDao.findByNameAndNotEqualMe(role.getCode(), role.getOid());
-        if (checkRole.isPresent()) {
-            throw new RobeRuntimeException("Code", role.getCode() + ALREADY_USED);
-        }
-        Role entity = roleDao.findById(role.getOid());
-        entity.setName(role.getName());
-        entity.setCode(role.getCode());
-        entity = roleDao.update(entity);
         return entity;
-
     }
 
     @POST
     @UnitOfWork
-    public Role create(@Auth Credentials credentials, @Valid Role role) {
-        Optional<Role> checkRole = roleDao.findByName(role.getCode());
-        if (checkRole.isPresent()) {
-            throw new RobeRuntimeException("Code", role.getCode() + ALREADY_USED);
-        }
-        return roleDao.create(role);
+    public Role create(@Auth Credentials credentials, @Valid Role model) {
+        return roleDao.create(model);
     }
 
-    @Path("group/{groupOid}/{roleOid}")
+    @Path("{id}")
     @PUT
     @UnitOfWork
-    public Role createRoleGroup(@Auth Credentials credentials, @PathParam("groupOid") String groupOid, @PathParam("roleOid") String roleOid) {
-        Role group = roleDao.findById(groupOid);
-        checkNotNull(group, "groupOid mustn't be null");
-        Role role = roleDao.findById(roleOid);
-        checkNotNull(role, "roleOid mustn't be null");
+    public Role update(@Auth Credentials credentials, @PathParam("id") String id, @Valid Role model) {
 
-        throw new RobeRuntimeException("Error", "createRoleGroup not implemented yet");
+        if (!id.equals(model.getOid())) {
+            throw new WebApplicationException(Response.status(412).build());
+        }
+        return roleDao.update(model);
     }
 
     @Path("{id}")
     @DELETE
     @UnitOfWork
-    public Role delete(@Auth Credentials credentials, @PathParam("id") String id, @Valid Role role) {
-        throw new RobeRuntimeException("Error", "delete not implemented yet");
+    public Role delete(@Auth Credentials credentials, @PathParam("id") String id, @Valid Role model) {
+        if (!id.equals(model.getOid())) {
+            throw new WebApplicationException(Response.status(412).build());
+        }
+        Role entity = roleDao.findById(id);
+        if (entity == null) {
+            throw new WebApplicationException(Response.status(404).build());
+        }
+        return roleDao.delete(entity);
     }
 
-    @Path("destroyRoleGroup/{groupOid}/{roleOid}")
-    @DELETE
-    @UnitOfWork
-    public Role destroyRoleGroup(@Auth Credentials credentials, @PathParam("groupOid") String groupOid, @PathParam("roleOid") String roleOid) {
-        Role group = roleDao.findById(groupOid);
-        Role role = roleDao.findById(roleOid);
-
-        throw new RobeRuntimeException("Error", "destroyRoleGroup not implemented yet");
-
-    }
 }
