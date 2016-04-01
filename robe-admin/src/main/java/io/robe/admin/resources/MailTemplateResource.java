099918a96478592393d55bfe2f8e60209ee1b838
Fatih Totrakanlı <fatih.totrakanli@mebitech.com>
2016-04-01 16:40:13 +0300

    adding javadoc and RobeService annotation

--- robe-admin/src/main/java/io/robe/admin/resources/MailTemplateResource.java
+++ robe-admin/src/main/java/io/robe/admin/resources/MailTemplateResource.java
@@ -7,6 +7,7 @@ import io.dropwizard.jersey.PATCH;
 import io.robe.admin.hibernate.dao.MailTemplateDao;
 import io.robe.admin.hibernate.entity.MailTemplate;
 import io.robe.auth.Credentials;
+import io.robe.common.service.RobeService;
 import io.robe.common.utils.FieldReflection;
 import org.hibernate.FlushMode;
 
@@ -26,12 +27,27 @@ public class MailTemplateResource {
     @Inject
     private MailTemplateDao mailTemplateDao;
 
+    /**
+     * Returns all {@link MailTemplate} as a collection with the related path.
+     *
+     * @param credentials auto fill by @{@link Auth} annotation for authentication.
+     * @return all @{@link MailTemplate} as a collection with the related path.
+     */
+    @RobeService(group = "MailTemplate", description = "Returns all MailTemplate as a collection")
     @GET
     @UnitOfWork(readOnly = true, cacheMode = GET, flushMode = FlushMode.MANUAL)
-    public List<MailTemplate> getAll() {
+    public List<MailTemplate> getAll(@Auth Credentials credentials) {
         return mailTemplateDao.findAll(MailTemplate.class);
     }
 
+    /**
+     * Returns a single {@link MailTemplate} related with the path and matches with the given id
+     *
+     * @param credentials auto fill by @{@link Auth} annotation for authentication.
+     * @param id          This is  the oid of {@link MailTemplate}
+     * @return a single @{@link MailTemplate} related with the path and matches with the given id.
+     */
+    @RobeService(group = "MailTemplate", description = "Returns a single MailTemplate related with the path and matches with the given id.")
     @Path("{id}")
     @GET
     @UnitOfWork(readOnly = true, cacheMode = GET, flushMode = FlushMode.MANUAL)
@@ -43,12 +59,29 @@ public class MailTemplateResource {
         return entity;
     }
 
+    /**
+     * Creates a single {@link MailTemplate} related with the path.
+     *
+     * @param credentials auto fill by @{@link Auth} annotation for authentication.
+     * @param model       This is the one model of {@link MailTemplate}
+     * @return Creates a single @{@link MailTemplate} related with the path
+     */
+    @RobeService(group = "MailTemplate", description = "Creates a single MailTemplate related with the path")
     @POST
     @UnitOfWork
     public MailTemplate create(@Auth Credentials credentials, @Valid MailTemplate model) {
         return mailTemplateDao.create(model);
     }
 
+    /**
+     * Updates a single {@link MailTemplate} related with the path and matches with the given id. Payload holds the whole data.
+     *
+     * @param credentials auto fill by @{@link Auth} annotation for authentication.
+     * @param id          This is  the oid of {@link MailTemplate}
+     * @param model       This is the one model of {@link MailTemplate}
+     * @return Updates a single @{@link MailTemplate} related with the path and matches with the given id.
+     */
+    @RobeService(group = "MailTemplate", description = "Updates a single MailTemplate related with the path and matches with the given id")
     @Path("{id}")
     @PUT
     @UnitOfWork
@@ -64,6 +97,17 @@ public class MailTemplateResource {
         return mailTemplateDao.update(model);
     }
 
+    /**
+     * Updates a single {@link MailTemplate} related with the path and matches with the given id.
+     * Payload will only contains update data.
+     * Version of the {@link MailTemplate} can be available at ETag in an If-Match header.
+     *
+     * @param credentials auto fill by @{@link Auth} annotation for authentication.
+     * @param id          This is  the oid of {@link MailTemplate}
+     * @param model       This is the one model of {@link MailTemplate}
+     * @return Updates a single @{@link MailTemplate} related with the path and matches with the given id.
+     */
+    @RobeService(group = "MailTemplate", description = "Updates a single @{@link MailTemplate} related with the path and matches with the given id")
     @Path("{id}")
     @PATCH
     @UnitOfWork
@@ -81,6 +125,15 @@ public class MailTemplateResource {
         return mailTemplateDao.update(model);
     }
 
+    /**
+     * Delete a single {@link MailTemplate} for the related path.
+     *
+     * @param credentials auto fill by @{@link Auth} annotation for authentication.
+     * @param id          This is  the oid of {@link MailTemplate}
+     * @param model       This is the one model of {@link MailTemplate}
+     * @return deletes a single @{@link MailTemplate} for the related path
+     */
+    @RobeService(group = "MailTemplate", description = "Deletes a single MailTemplate for the related path")
     @Path("{id}")
     @DELETE
     @UnitOfWork
