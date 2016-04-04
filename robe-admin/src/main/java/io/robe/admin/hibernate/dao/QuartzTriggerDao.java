1f5b70890d4f1540c6c7e6639450645bd125b0a4
hasanmumin <hasan.mumin@mebitech.com>
2016-04-04 16:56:10 +0300

    quartz job fixed

--- robe-admin/src/main/java/io/robe/admin/hibernate/dao/QuartzTriggerDao.java
+++ robe-admin/src/main/java/io/robe/admin/hibernate/dao/QuartzTriggerDao.java
@@ -1,13 +1,14 @@
 package io.robe.admin.hibernate.dao;
 
 import com.google.inject.Inject;
-import io.robe.hibernate.dao.BaseDao;
 import io.robe.admin.quartz.hibernate.TriggerEntity;
+import io.robe.hibernate.dao.BaseDao;
+import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 
-/**
- * Created by sinanselimoglu on 31/03/14.
- */
+import java.util.List;
+
 public class QuartzTriggerDao extends BaseDao<TriggerEntity> {
     /**
      * Constructor with session factory injection by guice
@@ -17,5 +18,13 @@ public class QuartzTriggerDao extends BaseDao<TriggerEntity> {
     @Inject
     public QuartzTriggerDao(SessionFactory sessionFactory) {
         super(sessionFactory);
+    }
+
+
+    public List<TriggerEntity> findByJobOid(String jobOid) {
+        Criteria criteria = currentSession().createCriteria(TriggerEntity.class);
+        criteria.add(Restrictions.eq("jobOid", jobOid));
+        return list(criteria);
+
     }
 }
