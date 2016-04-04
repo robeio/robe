b4c3744d3fa3697ea3d7d95b4a86b5350961fff8
EA <erhan.akbaba@mebitech.com>
2016-04-04 11:21:08 +0300

    adding javadoc and RobeService annotation

--- robe-admin/src/main/java/io/robe/admin/resources/ServiceResource.java
+++ robe-admin/src/main/java/io/robe/admin/resources/ServiceResource.java
@@ -6,7 +6,9 @@ import io.dropwizard.hibernate.UnitOfWork;
 import io.dropwizard.jersey.PATCH;
 import io.robe.admin.hibernate.dao.ServiceDao;
 import io.robe.admin.hibernate.entity.Service;
+import io.robe.admin.hibernate.entity.SystemParameter;
 import io.robe.auth.Credentials;
+import io.robe.common.service.RobeService;
 import io.robe.common.utils.FieldReflection;
 import org.hibernate.FlushMode;
 
@@ -27,12 +29,25 @@ public class ServiceResource {
     @Inject
     private ServiceDao serviceDao;
 
+    /**
+     * Returns all {@link Service ) as a collection.
+     * @param credentials Injected by {@link Auth} annotation for authentication.
+     * @return all {@link Service} as a collection.
+     */
+    @RobeService(group = "Service", description = "Returns all Services as a collection.")
     @GET
     @UnitOfWork(readOnly = true, cacheMode = GET, flushMode = FlushMode.MANUAL)
     public List<Service> getAll() {
         return serviceDao.findAll(Service.class);
     }
 
+    /**
+     * Return {@link Service) resource and matches with the given id.
+     * @param credentials Injected by {@link Auth} annotation for authentication.
+     * @param id This is  the oid of {@link Service}
+     * @return {@link Service} resource matches with the given id.
+     */
+    @RobeService(group = "Service", description = "Return Service resource.")
     @Path("{id}")
     @GET
     @UnitOfWork(readOnly = true, cacheMode = GET, flushMode = FlushMode.MANUAL)
@@ -44,12 +59,27 @@ public class ServiceResource {
         return entity;
     }
 
+    /**
+     * Create {@link Service) resource and matches with the given id.
+     * @param credentials Injected by {@link Auth} annotation for authentication.
+     * @param model This is the one model of {@link Service}
+     * @return Create {@link Service) resource and return given Service path link at header Location=example/{id].
+     */
+    @RobeService(group = "Service", description = "Create Service resource and return given Service path link at header Location=example/{id].")
     @POST
     @UnitOfWork
     public Service create(@Auth Credentials credentials, @Valid Service model) {
         return serviceDao.create(model);
     }
 
+    /**
+     * Update {@link Service) resource and matches with the given id.
+     * @param credentials Injected by {@link Auth} annotation for authentication.
+     * @param id This is  the oid of {@link Service}
+     * @param model This is the one model of {@link Service}
+     * @return Update {@link Service} resource and matches with the given id.
+     */
+    @RobeService(group = "Service", description = "Update Service resource and matches with the given id.")
     @Path("{id}")
     @PUT
     @UnitOfWork
@@ -64,6 +94,14 @@ public class ServiceResource {
         return serviceDao.update(model);
     }
 
+    /**
+     * Update {@link Service) resource and matches with the given id.
+     * @param credentials Injected by {@link Auth} annotation for authentication.
+     * @param id This is  the oid of {@link Service}
+     * @param model This is the one model of {@link Service}
+     * @return Update {@link Service) resource and matches with the given id.
+     */
+    @RobeService(group = "Service", description = "Update Service resource.")
     @PATCH
     @UnitOfWork
     @Path("{id}")
@@ -78,6 +116,14 @@ public class ServiceResource {
         return serviceDao.update(model);
     }
 
+    /**
+     * Delete {@link Service) resource and matches with the given id.
+     * @param credentials Injected by {@link Auth} annotation for authentication.
+     * @param id This is  the oid of {@link Service}
+     * @param model This is the one model of {@link Service}
+     * @return Delete {@link Service) resource.
+     */
+    @RobeService(group = "Service", description = "Delete Service resource.")
     @Path("{id}")
     @DELETE
     @UnitOfWork
