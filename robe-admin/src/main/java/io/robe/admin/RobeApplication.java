39a6bbd6e34d20a517669261424930a72efa8569
serayuzgur <serayuzgur@gmail.com>
2016-06-24 13:24:47 +0300

    SearchFrom version update

--- robe-admin/src/main/java/io/robe/admin/RobeApplication.java
+++ robe-admin/src/main/java/io/robe/admin/RobeApplication.java
@@ -25,14 +25,10 @@ import io.robe.guice.GuiceBundle;
 import io.robe.hibernate.RobeHibernateBundle;
 import io.robe.mail.MailBundle;
 import io.robe.quartz.QuartzBundle;
-import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
-import java.util.EnumSet;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -132,19 +128,6 @@ public class RobeApplication<T extends RobeConfiguration> extends Application<T>
                 environment.metrics());
         environment.getApplicationContext().addServlet(
                 new NonblockingServletHolder(new MetricsServlet()), "/metrics/*");
-
-        // Enable CORS headers
-        final FilterRegistration.Dynamic cors =
-                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
-
-        // Configure CORS parameters
-        cors.setInitParameter("allowedOrigins", "*");
-        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
-        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD,PATCH");
-        cors.setInitParameter("exposedHeaders", "X-Total-Count");
-
-        // Add URL mapping
-        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
     }
 
