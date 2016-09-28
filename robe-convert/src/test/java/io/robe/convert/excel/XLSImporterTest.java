9d440416f9e3a80da71941a06461170a452c69e6
hasanmumin <hasanmuminn@gmail.com>
2016-09-28 10:32:24 +0300

    Excel tests added

--- robe-convert/src/test/java/io/robe/convert/excel/XLSImporterTest.java
+++ robe-convert/src/test/java/io/robe/convert/excel/XLSImporterTest.java
@@ -2,6 +2,7 @@ package io.robe.convert.excel;
 
 import io.robe.convert.SamplePojo;
 import io.robe.convert.TestData;
+import io.robe.convert.common.OnItemHandler;
 import io.robe.convert.excel.importer.XLSImporter;
 
 import java.util.List;
@@ -29,5 +30,44 @@ public class XLSImporterTest {
             SamplePojo ref = TestData.getData().get(index++);
             assert item.equals(ref);
         }
+    }
+
+    @org.junit.Test
+    public void testImportStreamWithCharset() throws Exception {
+        XLSImporter<SamplePojo> xlsImporter = new XLSImporter(SamplePojo.class);
+        List<SamplePojo> list = xlsImporter.importStream(XLSImporterTest.class.getClassLoader().getResourceAsStream("sample.xls"), "UTF-8");
+        assert list.size() == TestData.getData().size();
+
+        int index = 0;
+        for (SamplePojo item : list) {
+            SamplePojo ref = TestData.getData().get(index++);
+            assert item.equals(ref);
+        }
+    }
+
+    @org.junit.Test
+    public void testImportStreamWithOnItemHandler() throws Exception {
+        XLSImporter<SamplePojo> xlsImporter = new XLSImporter(SamplePojo.class);
+
+        final int[] index = {0};
+        OnItemHandler<SamplePojo> onItemHandler = samplePojo -> {
+            SamplePojo ref = TestData.getData().get(index[0]++);
+            assert samplePojo.equals(ref);
+        };
+
+        xlsImporter.importStream(XLSImporterTest.class.getClassLoader().getResourceAsStream("sample.xls"), onItemHandler);
+    }
+
+    @org.junit.Test
+    public void testImportStreamWithOnItemHandlerWithCharset() throws Exception {
+        XLSImporter<SamplePojo> xlsImporter = new XLSImporter(SamplePojo.class);
+
+        final int[] index = {0};
+        OnItemHandler<SamplePojo> onItemHandler = samplePojo -> {
+            SamplePojo ref = TestData.getData().get(index[0]++);
+            assert samplePojo.equals(ref);
+        };
+
+        xlsImporter.importStream(XLSImporterTest.class.getClassLoader().getResourceAsStream("sample.xls"), onItemHandler, "UTF-8");
     }
 }
