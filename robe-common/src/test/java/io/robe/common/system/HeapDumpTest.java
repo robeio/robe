72cf7fe63e3c310f5120c4e9a0d183e71ac5c452
recep <recep.koseoglu@mebitech.com>
2016-10-01 21:13:47 +0300

    HeapDump test

--- robe-common/src/test/java/io/robe/common/system/HeapDumpTest.java
+++ robe-common/src/test/java/io/robe/common/system/HeapDumpTest.java
@@ -1,28 +1,18 @@
 package io.robe.common.system;
 
-import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 
-import java.io.File;
-
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.assertNotNull;
 
 /**
  * Created by recep on 01/10/16.
  */
 @FixMethodOrder
 public class HeapDumpTest {
-    File file;
-
-    @Before
-    public void setUp() throws Exception {
-        file = HeapDump.dump(true);
-
-    }
 
     @Test
     public void dump() throws Exception {
-        assertTrue(file.exists());
+        assertNotNull(HeapDump.dump(false));
     }
 }
