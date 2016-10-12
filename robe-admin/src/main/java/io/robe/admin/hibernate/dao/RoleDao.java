fe257003d32a1e2188bf18f7a7a9f8e96f1b2990
hasanmumin <hasanmuminn@gmail.com>
2016-10-12 15:09:02 +0300

    removing unused method

--- robe-admin/src/main/java/io/robe/admin/hibernate/dao/RoleDao.java
+++ robe-admin/src/main/java/io/robe/admin/hibernate/dao/RoleDao.java
@@ -5,9 +5,7 @@ import io.robe.admin.hibernate.entity.Role;
 import io.robe.auth.data.entry.RoleEntry;
 import io.robe.auth.data.store.RoleStore;
 import io.robe.hibernate.dao.BaseDao;
-import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
 
 import javax.inject.Inject;
 import java.util.Optional;
@@ -22,19 +20,5 @@ public class RoleDao extends BaseDao<Role> implements RoleStore {
 
     public Optional<? extends RoleEntry> findByRoleId(String oid) {
         return Optional.ofNullable(findById(oid));
-    }
-
-    public Optional<Role> findByName(String code) {
-        Criteria criteria = currentSession().createCriteria(Role.class);
-        criteria.add(Restrictions.eq("code", code));
-        return Optional.ofNullable(uniqueResult(criteria));
-    }
-
-    public Optional<Role> findByNameAndNotEqualMe(String code, String oid) {
-        Criteria criteria = currentSession().createCriteria(Role.class);
-        criteria.add(Restrictions.eq("code", code));
-        criteria.add(Restrictions.not(Restrictions.eq("oid", oid)));
-
-        return Optional.ofNullable(uniqueResult(criteria));
     }
 }
