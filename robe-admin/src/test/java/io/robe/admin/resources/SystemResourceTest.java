aab05df143908b70747506570567b5c49ecf9eba
hasanmumin <hasanmuminn@gmail.com>
2016-10-14 16:49:52 +0300

    SystemResourceTest.java added (need refactoring)

---
+++ robe-admin/src/test/java/io/robe/admin/resources/SystemResourceTest.java
@@ -0,0 +1,113 @@
+package io.robe.admin.resources;
+
+import io.robe.admin.util.request.TestRequest;
+import io.robe.admin.util.request.TestResponse;
+import io.robe.hibernate.entity.BaseEntity;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by hasanmumin on 14/10/2016.
+ */
+public class SystemResourceTest extends BaseResourceTest {
+    @Override
+    public String getPath() {
+        return "systems";
+    }
+
+    @Override
+    public Class getClazz() {
+        return null;
+    }
+
+    @Override
+    public void assertEquals(BaseEntity model, BaseEntity response) {
+
+    }
+
+    @Override
+    public void assertEquals(BaseEntity mergeInstance, BaseEntity original, BaseEntity response) {
+
+    }
+
+    @Override
+    public BaseEntity instance() {
+        return null;
+    }
+
+    @Override
+    public BaseEntity update(BaseEntity response) {
+        return null;
+    }
+
+    @Override
+    public BaseEntity mergeInstance() {
+        return null;
+    }
+
+    @Override
+    public void getAll() throws Exception {
+    }
+
+    @Override
+    public void get() throws Exception {
+    }
+
+    @Override
+    public void create() throws Exception {
+    }
+
+
+    @Override
+    public void update() throws Exception {
+    }
+
+    @Override
+    public void updateShouldThrowWebApplicationException1() throws Exception {
+    }
+
+    @Override
+    public void updateShouldThrowWebApplicationException2() throws Exception {
+    }
+
+
+    @Override
+    public void delete() throws Exception {
+    }
+
+    @Override
+    public void deleteShouldThrowWebApplicationException1() throws Exception {
+    }
+
+    @Override
+    public void deleteShouldThrowWebApplicationException2() throws Exception {
+    }
+
+    @Override
+    public void merge() throws Exception {
+    }
+
+    @Override
+    public void mergeShouldThrowWebApplicationException1() throws Exception {
+    }
+
+    @Override
+    public void mergeShouldThrowWebApplicationException2() throws Exception {
+    }
+
+    @Test
+    public void getHeapDump() throws IOException {
+        TestRequest request = requestBuilder.endpoint("heapdump").build();
+
+
+        try {
+            TestResponse response = client.get(request); // TODO handle this response file
+        } catch (Exception e) {
+            // TODO ignore ?
+        }
+
+        Assert.assertTrue(true);
+    }
+}
