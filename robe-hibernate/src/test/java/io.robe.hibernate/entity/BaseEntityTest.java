76f6a99740932869ca56123394eea2d59ddd5625
ademozay <ozayadem@gmail.com>
2016-10-19 15:08:06 +0300

    test of BaseEntity

---
+++ robe-hibernate/src/test/java/io.robe.hibernate/entity/BaseEntityTest.java
@@ -0,0 +1,38 @@
+package io.robe.hibernate.entity;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * Created by adem on 19/10/2016.
+ */
+public class BaseEntityTest {
+
+    private String oid = "oid";
+    private long lastUpdated = new Date().getTime();
+
+    class Entity extends BaseEntity {
+        public Entity() {
+            super(oid, lastUpdated);
+        }
+    }
+
+    @Test
+    public void constructor() {
+        Entity entity = new Entity();
+        Assert.assertEquals(oid, entity.getOid());
+        Assert.assertEquals(lastUpdated, entity.getLastUpdated());
+    }
+
+    @Test
+    public void updateLastUpdated() {
+        Entity entity = new Entity();
+        lastUpdated = new Date().getTime();
+        entity.setLastUpdated(lastUpdated);
+        Assert.assertEquals(oid, entity.getOid());
+        Assert.assertEquals(lastUpdated, entity.getLastUpdated());
+    }
+
+}
