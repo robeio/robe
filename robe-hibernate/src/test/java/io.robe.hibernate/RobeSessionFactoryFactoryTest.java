67f3e2828463b531f2ed7261fc4018f2ad4c794c
ademozay <ozayadem@gmail.com>
2016-10-26 11:48:54 +0300

    added test for RobeSessionFactoryFactory

---
+++ robe-hibernate/src/test/java/io.robe.hibernate/RobeSessionFactoryFactoryTest.java
@@ -0,0 +1,23 @@
+package io.robe.hibernate;
+
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by adem on 26/10/2016.
+ */
+public class RobeSessionFactoryFactoryTest {
+
+    @Test
+    public void configure() {
+        Configuration configuration = mock(Configuration.class);
+        RobeSessionFactoryFactory factory = new RobeSessionFactoryFactory();
+        when(configuration.getProperty("hibernate.prefix")).thenReturn("P_");
+        when(configuration.getProperty("hibernate.namingStrategy")).thenReturn("P_");
+        factory.configure(configuration, null);
+    }
+
+}
