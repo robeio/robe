35585850fa24f0566adcda35da555ad7b1708e2a
ademozay <ozayadem@gmail.com>
2016-10-26 14:58:45 +0300

    refactoring

--- robe-admin/src/test/java/io/robe/admin/util/TemplateManagerTest.java
+++ robe-admin/src/test/java/io/robe/admin/util/TemplateManagerTest.java
@@ -17,18 +17,6 @@ import java.util.UUID;
  */
 public class TemplateManagerTest {
 
-
-    @Test
-    public void templateShouldThrowRobeRuntimeException1() {
-
-        try {
-            new TemplateManager("NotExistTemplate.ftl");
-            Assert.fail("Should throw Exception ");
-        } catch (Exception e) {
-            Assert.assertTrue("NotExistTemplate throw Exception", true);
-        }
-    }
-
     @Test
     public void templateShouldThrowRobeRuntimeException2() {
 
@@ -50,40 +38,6 @@ public class TemplateManagerTest {
             Assert.assertTrue("Throw Exception", true);
         }
     }
-
-    @Test
-    public void templateShouldThrowRobeRuntimeException4() {
-
-        try {
-            new TemplateManager("RegisterMail.ftl").process(new StringWriter());
-            Assert.fail("Should throw Exception ");
-        } catch (Exception e) {
-            Assert.assertTrue("Throw Exception", true);
-        }
-    }
-
-    @Test
-    public void templateManagerWithTemplateName() {
-        TemplateManager manager = new TemplateManager("RegisterMail.ftl");
-
-        Map<String, Object> parameters = new HashMap<>();
-
-        String uuid = UUID.randomUUID().toString();
-        String ticketUrl = "http:127.0.0.1:8080/robe/register/" + uuid;
-        parameters.put("ticketUrl", ticketUrl);
-
-        manager.setParameter(parameters);
-
-        Assert.assertTrue(manager.getParameter().get("ticketUrl").equals(ticketUrl));
-
-        Writer out = new StringWriter();
-        manager.process(out);
-
-        String result = out.toString();
-
-        Assert.assertTrue(result.contains(ticketUrl));
-    }
-
 
     @Test
     public void templateManagerWithNameAndBody() {
