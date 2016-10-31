7b32ca3742f3c1ebc8d9f9506bc2fb41a1050c25
serayuzgur <serayuzgur@gmail.com>
2016-10-31 12:08:40 +0300

    Character enum added for Parsers. ParsersText

--- robe-convert/src/main/java/io/robe/convert/xml/parsers/Parsers.java
+++ robe-convert/src/main/java/io/robe/convert/xml/parsers/Parsers.java
@@ -9,7 +9,8 @@ public enum Parsers {
     INTEGER("java.lang.Integer", ParseInt.class),
     LONG("java.lang.Long", ParseLong.class),
     STRING("java.lang.String", ParseString.class),
-    CHAR("java.lang.String", ParseChar.class),
+    CHAR("char", ParseChar.class),
+    CHARACTER("java.lang.Character", ParseChar.class),
     DATE("java.util.Date", ParseDate.class),
     ENUM("java.util.Enum", ParseEnum.class);
 
@@ -22,7 +23,8 @@ public enum Parsers {
             return (parser == null) ? null : parser.newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
-        }    }
+        }
+    }
 
     private Parsers(String type, Class<? extends IsParser> parser) {
         this.type = type;
